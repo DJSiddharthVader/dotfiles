d910f7ceaf8c7df2a6772892ac769bb67f24adcf
Sid Reed <sidreed34@gmail.com>
2018-10-28 00:35:40 -0400

    changed monitor resolution

--- .scripts/togglescreen.sh
+++ .scripts/togglescreen.sh
@@ -4,7 +4,7 @@ if [ $1 = 'vga' ]
 then
     if [ $2 -eq 1 ];
     then
-        xrandr --output eDP-1 --output DP-2 --mode 1600x900 --right-of eDP-1
+        xrandr --output eDP-1 --output DP-2 --mode 1360x768 --right-of eDP-1
         ~/.fehbg &
         i3-msg restart
     else
