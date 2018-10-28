d9863556f31a3abdf61745db1a26329e61b62064
Sid Reed <sidreed34@gmail.com>
2018-10-28 00:48:41 -0400

    actually works now, but only right padded for songs shorter than scroll size

--- .scripts/mpdscroll.sh
+++ .scripts/mpdscroll.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
+#Script to scroll song name of current mpd song in polybar
 
 if ! mpc >/dev/null 2>&1; then
     echo Server offline
     exit 1
 elif mpc status | grep -q playing; then
-    #( mpc current  | zscroll -l 20 -d 0.30 -b " " -a " " -p " " | sed -e :a -ue 's/^.\{1,20\}$/& /;ta'| sed -uEn 's/(^.*$)/───|\1|───/p' ) &
-    ( zscroll -l 20 -d 0.30 -b "" -a "" -p " /// " -u true "mpc current"  | sed -e :a -ue 's/^.\{1,20\}$/& /;ta'| sed -uEn 's/(^.*$)/───| \1|───/p' ) &
-    #( mpc current | sed -e :a -ue 's/^.\{1,20\}$/& /;ta' | sed -uEn 's/(^.*$)/───| \1 |───/p' | zscroll -l 20 -d 0.25 -p " " ) &
+    ( zscroll -l 20 -d 0.30 -b "" -a "" -p " /// " -u true "mpc current"  | sed -e :a -ue 's/^.\{1,20\}$/& /;ta' | sed -uEn 's/(^.*$)/───|\1|───/p' ) &
+    #( zscroll -l 20 -d 0.30 -b "" -a "" -p " /// " -u true "mpc current"  use zscroll to scroll the output of mpc current with /// as the delim between the end of text
+    #| sed -e :a -ue 's/^.\{1,20\}$/& /;ta'#pad with space to be 20 chars if not
+    #| sed -uEn 's/(^.*$)/───| \1|───/p' surround text in ───| |─── for asthetic
+    #) & capture, run in background
 elif mpc status | grep -q paused; then
     echo "───| Paused |───"
-    #echo "   Paused   "
 else
     echo "───| Stopped |───"
-    #echo "Not playing"
 fi
-# Block until an event is emitted
+
 mpc idle >/dev/null
 
