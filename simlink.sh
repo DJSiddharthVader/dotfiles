550034b1c2841bf8c2e9eea3b24ffd6821a3025d
Sid Reed <sidreed34@gmail.com>
2018-10-28 00:38:52 -0400

    fixed

--- simlink.sh
+++ simlink.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
-ln -s ~/dotfiles/.vim ~/.vim
-ln -s ~/dotfiles/.vimrc ~/.vimrc
+#\(\..\{-}\)\( \)\@=.\{-}\(dotfiles\/\)
+#%s//\1 \~\/\1/
+ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
+ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.config ~/.config
 ln -s ~/dotfiles/.dircolors_256 ~/.dircolors_256
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.inputrc ~/.inputrc
+ln -s ~/dotfiles/.ipython ~/.ipython
+ln -s ~/dotfiles/.jupyter ~/.jupyter
+ln -s ~/dotfiles/.mpd ~/.mpd
+ln -s ~/dotfiles/.ncmcpp ~/.ncmcpp
+ln -s ~/dotfiles/.ncmpcpp ~/.ncmpcpp
+ln -s ~/dotfiles/.profile ~/.profile
+ln -s ~/dotfiles/.scripts ~/.scripts
+ln -s ~/dotfiles/.steampath -> /home/sidreed/
+ln -s ~/dotfiles/.steampid -> /home/sidreed/
 ln -s ~/dotfiles/.tmux ~/.tmux
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/.tmux_prompt.sh ~/.tmux_prompt.sh
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
+ln -s ~/dotfiles/.tmux
+ln -s ~/dotfiles/.tmux_prompt
+ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.Xdefaults ~/.Xdefaults
+ln -s ~/dotfiles/.Xresources ~/.Xresources
