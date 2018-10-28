f1535139a6a5f83fab354482af1cbe717c680883
Sid Reed <sidreed34@gmail.com>
2018-10-28 00:48:05 -0400

    fixed

--- .scripts/wallpaper.sh
+++ .scripts/wallpaper.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
 walp=~/dotfiles/wallpapers/`ls  ~/dotfiles/wallpapers | shuf -n 1`
-wal -i $walp
-feh --bg-scale $walp --bg-scale $walp
+
+case "$1" in
+    'font')
+        wal -e -n -i $walp #font only
+        ;;
+    'background')
+        feh --bg-scale $walp --bg-scale $walp #background only
+        ;;
+    'both')
+        wal -e -i $walp #set both
+        ;;
+    *)
+        echo "Usage: $0 {font|background|both}"
+        exit 1
+esac
+
+
