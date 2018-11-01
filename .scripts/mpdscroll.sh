2eddef4792d4ebe55ec91673afdb4299b7341e82
Sid Reed <sidreed34@gmail.com>
2018-10-31 20:05:14 -0400

    trying to fix padding, still not working

--- .scripts/mpdscroll.sh
+++ .scripts/mpdscroll.sh
@@ -5,7 +5,8 @@ if ! mpc >/dev/null 2>&1; then
     echo Server offline
     exit 1
 elif mpc status | grep -q playing; then
-    ( zscroll -l 20 -d 0.30 -b "" -a "" -p " /// " -u true "mpc current"  | sed -e :a -ue 's/^.\{1,20\}$/& /;ta' | sed -uEn 's/(^.*$)/───|\1|───/p' ) &
+    #( zscroll -l 20 -d 0.30 -b "" -a "" -p " /// " -u true "mpc current"  | sed -e :a -ue 's/^.\{1,20\}$/& /;ta' | sed -uEn 's/(^.*$)/───| \1|───/p' ) &
+    ( zscroll -l 20 -d 0.30 -b "" -a "" -p " /// " -u true "mpc current"  | sed -e :a -ue 's/^.\{1,20\}$/& /;ta' | sed -uEn 's/(^.*$)/───| \1|───/p' ) &
     #( zscroll -l 20 -d 0.30 -b "" -a "" -p " /// " -u true "mpc current"  use zscroll to scroll the output of mpc current with /// as the delim between the end of text
     #| sed -e :a -ue 's/^.\{1,20\}$/& /;ta'#pad with space to be 20 chars if not
     #| sed -uEn 's/(^.*$)/───| \1|───/p' surround text in ───| |─── for asthetic
