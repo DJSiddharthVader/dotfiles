130d2473cd2d61d4165904e46bf52e661367aea6
Sid Reed <sidreed34@gmail.com>
2018-11-05 20:53:15 -0500

    added total tasks

--- .scripts/task_counter.sh
+++ .scripts/task_counter.sh
@@ -2,10 +2,10 @@
 
 tskfile=~/dotfiles/tasks/tasks
 task_cats=$(grep -o -P '^.*?:' "$tskfile" | sort -u)
-#echo "$task_cats"
-outp='Tasks |'
+total=$(cat "$tskfile" | wc -l)
+outp="Tasks:$total|"
 while read -r catg; do
-    count=`grep "^$catg" "$tskfile" | wc -l`
+    count=`grep "^$catg" "$tskfile" | wc -l | cut -d' ' -f2`
     outp+="$catg$count|"
 done <<< "$task_cats"
 echo "${outp::-1}|"
