513252afa6abebe6784ddf9420bb80520ad9ec7b
Sid Reed <sidreed34@gmail.com>
2018-11-08 21:05:38 -0500

    finaly actually fixed song space padding issue

--- .scripts/mpdscroll.sh
+++ .scripts/mpdscroll.sh
@@ -5,12 +5,11 @@ if ! mpc >/dev/null 2>&1; then
     echo Server offline
     exit 1
 elif mpc status | grep -q playing; then
-    #( zscroll -l 20 -d 0.30 -b "" -a "" -p " /// " -u true "mpc current"  | sed -e :a -ue 's/^.\{1,20\}$/& /;ta' | sed -uEn 's/(^.*$)/───| \1|───/p' ) &
-    ( zscroll -l 20 -d 0.30 -b "" -a "" -p " /// " -u true "mpc current"  | sed -e :a -ue 's/^.\{1,20\}$/& /;ta' | sed -uEn 's/(^.*$)/───| \1|───/p' ) &
-    #( zscroll -l 20 -d 0.30 -b "" -a "" -p " /// " -u true "mpc current"  use zscroll to scroll the output of mpc current with /// as the delim between the end of text
-    #| sed -e :a -ue 's/^.\{1,20\}$/& /;ta'#pad with space to be 20 chars if not
-    #| sed -uEn 's/(^.*$)/───| \1|───/p' surround text in ───| |─── for asthetic
-    #) & capture, run in background
+    if [ `mpc current | wc -c` -gt 20 ]; then
+        ( zscroll -l 20 -d 0.30 -b "" -a "" -p " /// " -u true "mpc current"  | sed -e :a -ue "s/^.\{1,20\}$/& /;ta" | sed -uEn 's/(^.*$)/───| \1|───/p' ) &
+    else
+        ( echo `mpc current` | sed -uEn 's/(^.*$)/───|\1|───/p' ) &
+    fi
 elif mpc status | grep -q paused; then
     echo "───| Paused |───"
 else
@@ -19,3 +18,7 @@ fi
 
 mpc idle >/dev/null
 
+    #( zscroll -l 20 -d 0.30 -b "" -a "" -p " /// " -u true "mpc current"  use zscroll to scroll the output of mpc current with /// as the delim between the end of text
+    #| sed -e :a -ue 's/^.\{1,20\}$/& /;ta'#pad with space to be 20 chars if not
+    #| sed -uEn 's/(^.*$)/───| \1|───/p' surround text in ───| |─── for asthetic
+    #) & capture, run in background
