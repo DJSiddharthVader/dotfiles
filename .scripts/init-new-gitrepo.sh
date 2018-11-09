0e7774a1592a9504cab1898dbf4fe53ce275db21
Sid Reed <sidreed34@gmail.com>
2018-11-08 20:35:08 -0500

    fixed, now works because I set up API key

--- .scripts/init-new-gitrepo.sh
+++ .scripts/init-new-gitrepo.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#$1 is new repor name
+#$1 is new repo name
 #$2 is a one sentence description in single quotes
 
 user='DJSIddharthVader'
 
 mkdir -p "$1"
 dflag='{"name":'"\"$1\""}
-#curl -u "$user" https://api.github.com/user/repos -d "$dflag"
+curl -u "$user" https://api.github.com/user/repos -d "$dflag"
 echo "curl -u $user https://api.github.com/user/repos -d $dflag"
 cd "$1"
 
 git init
-search="github.com"
-replace="$user@github.com"
-sed -i -e "s/$search/$replace/" ./.git/config
+#search="github.com"
+#replace="$user@github.com"
+#sed -i -e "s/$search/$replace/" ./.git/config
 
 echo "$2" > README.md
 git add README.md
@@ -22,3 +22,4 @@ git commit -m "first commit"
 originurl="git@github.com:$user/$1.git"
 git remote add origin "$originurl"
 git push -u origin master
+git config remote.origin.push HEAD
