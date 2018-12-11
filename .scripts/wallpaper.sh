37e2cb538e5da0f907dc897c815d37bfad0bdee7
Sid Reed <sidreed34@gmail.com>
2018-12-11 15:25:58 -0500

    merged imagemagik commands, little faster now

--- .scripts/wallpaper.sh
+++ .scripts/wallpaper.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+i3dir="/home/sidreed/dotfiles/.config/i3/" #store bordered image
+resolution='1366x768!' #resolution, ignore aspect ratio
+
 imgname=`ls  ~/Pictures/wallpapers | shuf -n 1` #pick rnd image
 if [ "$2" = "" ]; then
     imgname=`ls  ~/Pictures/wallpapers | shuf -n 1` #pick rnd image
@@ -8,32 +11,33 @@ else
     imgname=`basename "$2"`
 fi
 walp="/home/sidreed/Pictures/wallpapers/$imgname" #need abs paths or convert fails
-i3dir="/home/sidreed/dotfiles/.config/i3/" #store bordered image
-resolution='1366x768!' #resolution, ignore aspect ratio
 
 case "$1" in
     'font')
         wal -e -n -i $walp & #font only
         ;;
-    'background')
+    'back')
         bgfile="$i3dir"bordered_background.png
-        convert "$walp" -resize "$resolution" "$bgfile"
-        convert "$bgfile" -bordercolor Black -border 0x5% "$bgfile"
+#        convert "$walp" -resize "$resolution" "$bgfile"
+#        convert "$bgfile" -bordercolor Black -border 0x5% "$bgfile"
+        convert "$walp" -resize "$resolution" -bordercolor Black -border 0x5% "$bgfile"
         feh --bg-scale "$bgfile"
         cp "$walp" ~/dotfiles/.config/i3/unbordered_background.png
         ;;
     'both')
-        wal -e -n -i $walp & #font only
         bgfile="$i3dir"bordered_background.png
-        convert "$walp" -resize "$resolution" "$bgfile"
-        convert "$bgfile" -bordercolor Black -border 0x5% "$bgfile"
+#        convert "$walp" -resize "$resolution" "$bgfile"
+#        convert "$bgfile" -bordercolor Black -border 0x5% "$bgfile"
+        convert "$walp" -resize "$resolution" -bordercolor Black -border 0x5% "$bgfile"
         feh --bg-scale "$bgfile"
+        wal -e -n -i $walp & #font only
         cp "$walp" ~/dotfiles/.config/i3/unbordered_background.png
         ;;
     'sback')
         bgfile="$i3dir"bordered_background.png
-        convert "$walp" -resize "$resolution" "$bgfile"
-        convert "$bgfile" -bordercolor Black -border 0x5% "$bgfile"
+#        convert "$walp" -resize "$resolution" "$bgfile"
+#        convert "$bgfile" -bordercolor Black -border 0x5% "$bgfile"
+        convert "$walp" -resize "$resolution" -bordercolor Black -border 0x5% "$bgfile"
         feh --bg-scale "$bgfile"
         echo "feh --bg-scale $walp" >> .fehbg
         echo "feh --bg-scale $bgfile" >> .fehbg
