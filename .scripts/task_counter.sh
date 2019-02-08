846bf66a6e5695b85e9c80169271557ded3cb65e
Sid Reed <sidreed34@gmail.com>
2019-02-08 18:51:29 -0500

    more posix compliant for polybr-scripts repo

--- .scripts/task_counter.sh
+++ .scripts/task_counter.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-
-tskfile=~/dotfiles/tasks/tasks
-task_cats=$(cat "$tskfile" | cut -d':' -f1 | sort | uniq -c | sort -n)
-total=$(cat "$tskfile" | wc -l)
+tskfile="$HOME/dotfiles/tasks/tasks"
+task_cats=$(cut -d':' -f1 "$tskfile" | sort | uniq -c | sort -n)
+total=$(wc -l "$tskfile" | cut -d" " -f1)
 outp="Tasks:$total|"
 while read -r catg; do
-    catg=`echo "$catg" | cut -d' ' -f2`
-    count=`grep "^$catg" "$tskfile" | wc -l | cut -d' ' -f2`
+    catg=$(echo "$catg" | cut -d' ' -f2)
+    count=$(grep -c "^$catg" "$tskfile" | cut -d' ' -f2)
     outp+="$catg:$count|"
 done <<< "$task_cats"
 echo "${outp::-1}|"
+
+#tskfile=~/dotfiles/tasks/tasks
+#task_cats=$(cat "$tskfile" | cut -d':' -f1 | sort | uniq -c | sort -n)
+#total=$(cat "$tskfile" | wc -l)
+#outp="Tasks:$total|"
+#while read -r catg; do
+#    catg=`echo "$catg" | cut -d' ' -f2`
+#    count=`grep "^$catg" "$tskfile" | wc -l | cut -d' ' -f2`
+#    outp+="$catg:$count|"
+#done <<< "$task_cats"
+#echo "${outp::-1}|"
