d6ee063336f58fce780d2b5b23ad231faae9838a
Sid Reed <sidreed34@gmail.com>
2019-02-08 20:39:05 -0500

    added toggle function, rewrote with functions

--- .scripts/togglebar.sh
+++ .scripts/togglebar.sh
@@ -1,20 +1,64 @@
 #!/bin/bash
-
-if [ "$1" = 'on' ]; then
-    killall -q polybar
+togglefile="$HOME/.config/.bartoggle"
+function fullbar() {
     feh --bg-scale /home/sidreed/dotfiles/.config/i3/bordered_background.png
     ~/.scripts/polybar_launch
-elif [ "$1" = 'off' ]; then
+}
+function minibar() {
     killall -q polybar
     feh --bg-scale /home/sidreed/dotfiles/.config/i3/unbordered_background.png
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
         MONITOR=$m polybar minimal &
     done
-elif [ "$1" = 'none' ]; then
+}
+function nobar() {
     killall -q polybar
     feh --bg-scale /home/sidreed/dotfiles/.config/i3/unbordered_background.png
+}
+function main() {
+    #toggle or explicit
+    if [ "$1" = 'toggle' ]; then
+        option=$(cat "$togglefile")
+    else
+        option="$1"
+    fi
+    #set bar
+    case "$option" in
+        full)
+            fullbar
+            echo "mini" >| "$togglefile"
+            ;;
+        mini)
+            minibar
+            echo "none" >| "$togglefile"
+            ;;
+        none)
+            nobar
+            echo "full" >| "$togglefile"
+            ;;
+        *)
+            echo "Usage: $0 {toggle|full||mini|none}"
+            exit 1
+    esac
+}
+main "$1"
 
-else
-    echo "error: arg 1 is on or off"
-fi
-
+#DEPRECIATED
+#elif [ "$1" = 'on' ]; then
+#    killall -q polybar
+#    feh --bg-scale /home/sidreed/dotfiles/.config/i3/bordered_background.png
+#    ~/.scripts/polybar_launch
+#elif [ "$1" = 'off' ]; then
+#    killall -q polybar
+#    feh --bg-scale /home/sidreed/dotfiles/.config/i3/unbordered_background.png
+#    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#        MONITOR=$m polybar minimal &
+#    done
+#elif [ "$1" = 'none' ]; then
+#    killall -q polybar
+#    feh --bg-scale /home/sidreed/dotfiles/.config/i3/unbordered_background.png
+#
+#else
+#    echo "error: arg 1 is on or off"
+#fi
+#
