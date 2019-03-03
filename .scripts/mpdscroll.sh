c95be1c107b0564ca1764d1c0166c409273764ab
Sid Reed <sidreed34@gmail.com>
2019-03-02 21:00:49 -0500

    switched bookends to music symbol

--- .scripts/mpdscroll.sh
+++ .scripts/mpdscroll.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
-#Script to scroll song name of current mpd song in polybar
+#Script to scroll song name of current mpd song in polybar
 thresh=20
 if ! mpc >/dev/null 2>&1; then
     echo Server offline
     exit 1
 elif mpc status | grep -q playing; then
     if [ "$(mpc current | wc -c)" -gt "$thresh" ]; then
-        ( zscroll -l "$thresh" -d 0.30 -b "" -a "" -p " /// " -u true "mpc current"  | sed -e :a -ue "s/^.\{1,$thresh\}$/& /;ta" | sed -uEn 's/(^.*$)/───| \1|───/p' ) &
+        ( zscroll -l "$thresh" -d 0.30 -b "" -a "" -p " /// " -u true "mpc current"  | sed -e :a -ue "s/^.\{1,$thresh\}$/& /;ta" | sed -uEn 's/(^.*$)/ \1/p' ) &
     else
-        (echo "$(mpc current)" | sed -uEn 's/(^.*$)/───|\1|───/p') &
+        (echo "$(mpc current)" | sed -uEn 's/(^.*$)/ \1 /p') &
     fi
 elif mpc status | grep -q paused; then
-    echo "───| Paused |───"
+    echo " Paused "
 else
-    echo "───| Stopped |───"
+    echo " Stopped "
 fi
 
 mpc idle >/dev/null
