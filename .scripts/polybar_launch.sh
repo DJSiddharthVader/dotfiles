e06d33cb373d8c3df2581af82f2c3d73f1b2a376
Sid Reed <sidreed34@gmail.com>
2019-03-02 21:05:37 -0500

    rewrote for simplicity, now resets background image with feh

--- .scripts/polybar_launch.sh
+++ .scripts/polybar_launch.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-function launchbar() {
-    killall -q polybar # Terminate already running bar instances
-    while pgrep -x polybar >/dev/null; do sleep 1; done # Wait until the processes have been shut down
+function multiscreen() {
     case "$1" in
         full)
-            for m in $(xrandr --query | grep " connected" | cut -d" " -f1);
+            for m in $(xrandr | grep ' connected' | cut -d' ' -f1)
             do
                 MONITOR=$m polybar lap -q &
                 MONITOR=$m polybar schedule -q &
             done
             ;;
         mini)
-            for m in $(xrandr --query | grep " connected" | cut -d" " -f1);
+            for m in $(xrandr | grep ' connected' | cut -d' ' -f1)
             do
                 MONITOR=$m polybar minimal -q &
             done
@@ -21,15 +19,112 @@ function launchbar() {
             sleep 1
             ;;
         *)
-            echo "usage $0 {toggle|full|mini|none}"
+            echo "usage $0 {full|mini|none}"
             exit 1
             ;;
     esac
 }
 
+function singelscreen() {
+    MONITOR=$(xrandr --query | grep " connected" | cut -d" " -f1)
+    case "$1" in
+        full)
+            MONITOR=$m polybar lap -q &
+            MONITOR=$m polybar schedule -q &
+            ;;
+        mini)
+            MONITOR=$m polybar minimal -q &
+            ;;
+        none)
+            sleep 1
+            ;;
+        *)
+            echo "usage $0 {full|mini|none}"
+            exit 1
+            ;;
+    esac
+}
+
+function launchbar_dep() {
+    killall -q polybar # Terminate already running bar instances
+    #while pgrep -x polybar >/dev/null; do sleep 1; done # Wait until the processes have been shut down
+    if [ "$(xrandr | grep -c " connected")" -gt 1 ]; then #multi monitors
+        case "$1" in
+            full)
+                multiscreen full
+                ;;
+            mini)
+                multiscreen mini
+                ;;
+            none)
+                multiscreen none
+                ;;
+            *)
+                echo "usage $0 {full|mini|none}"
+                exit 1
+                ;;
+        esac
+    else #single monitor
+        case "$1" in
+            full)
+                singlescreen full
+                ;;
+            mini)
+                singlescreen mini
+                ;;
+            none)
+                singlescreen none
+                ;;
+            *)
+                echo "usage $0 {full|mini|none}"
+                exit 1
+                ;;
+        esac
+    fi
+}
+
+function setWallPaper() {
+    bordered="$(head -1 $HOME/dotfiles/.bartoggle)"
+    case "$bordered" in
+        full)
+            feh --bg-scale /home/sidreed/dotfiles/.config/i3/bordered_background.png
+            ;;
+        mini)
+            feh --bg-scale /home/sidreed/dotfiles/.config/i3/unbordered_background.png
+            ;;
+        none)
+            feh --bg-scale /home/sidreed/dotfiles/.config/i3/unbordered_background.png
+            ;;
+        *)
+            exit 1
+            ;;
+    esac
+}
+
+function launchbar() {
+    killall -q polybar # Terminate already running bar instances
+    #while pgrep -x polybar >/dev/null; do sleep 1; done # Wait until the processes have been shut down
+    case "$1" in
+        full)
+            multiscreen full
+            ;;
+        mini)
+            multiscreen mini
+            ;;
+        none)
+            multiscreen none
+            ;;
+        *)
+            echo "usage $0 {full|mini|none}"
+            exit 1
+            ;;
+    esac
+    setWallPaper
+}
+
 function main() {
     if [ -z "$1" ]; then #are args given
-        launchbar "$(head -1 ~/.config/.bartoggle)"
+        launchbar "$(head -1 ~/dotfiles/.bartoggle)"
     else
         launchbar "$1"
     fi
