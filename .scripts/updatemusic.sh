e08de7459e4de4f8f4da8eac65822c6bb7a580bb
Sid Reed <sidreed34@gmail.com>
2019-03-13 14:01:54 -0400

    now also pulls from soundcloud

--- .scripts/updatemusic.sh
+++ .scripts/updatemusic.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+scdl --download-archive /home/sidreed/dotfiles/.config/soundcloud_download_archive.txt -c -l https://soundcloud.com/sid-reed-871359466/sets/likedsongs
 youtube-dl  --audio-quality 0 \
             --audio-format mp3 \
             --prefer-ffmpeg \
             -x \
             --yes-playlist \
-            --verbose \
             -i \
-            --download-archive ~/.config/youtube-dl_downloads.txt \
+            --download-archive /home/sidreed/dotfiles/.config/youtube-dl_downloads.txt \
             "https://www.youtube.com/playlist?list=PLMU-V2Iwwq69O1QA3kOtw_YTQ2q-sD-gT"
