0f786f81fdae75ed834b952257df9e9ae4e78027
Sid Reed <sidreed34@gmail.com>
2019-03-13 14:02:20 -0400

    added checks for valid file

--- .scripts/knitpdf.sh
+++ .scripts/knitpdf.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #check valid file provided
 if [ $# -eq 0 ]; then
     base="main"
@@ -8,8 +7,9 @@ if [ $# -eq 0 ]; then
 else
     file="$1"
     #check file exists
-    if [ ! -f "$file " ]; then
+    if [ ! -f "$file" ]; then
         echo "file $1 not found"
+        exit 1
     fi
     #check extension is .Rnw
     extension="$(basename $file | cut -f2 -d'.')"
@@ -17,18 +17,26 @@ else
         base="$(basename $file | cut -f1 -d'.')"
     else
         echo "invalid file extension ($extension), file must be .Rnw file"
+        exit 1
     fi
 fi
 #Compiling file
-if [[ $(which latexmk) ]]; then
+#using latexmk
+if [ -x "$(command -v latexmk)" ];then
+    echo "latexmk installed, using latexmk"
     latexmk -c
-    R -q -e "library('knitr'); knitr::knit('$1')"
-    latexmk -pdf "$base"
-    latexmk -pdf  "$base"
-else
-    R -q -e "library('knitr'); knitr::knit('$1')"
-    pdflatex "$base".tex
-    biber "$base"
-    pdflatex "$base".tex
-    pdflatex "$base".tex
+    R -q -e "library('knitr'); knitr::knit('$base.Rnw')"
+    latexmk -pdf "$base".tex
+    latexmk -pdf  "$base".tex
+else #using pdflatex and biber
+    if [[ ( -x "$(command -v pdflatex)" ) && ( -x "$(command -v biber)" ) ]];then
+        echo "latexmk not installed, using pdflatex, biber"
+        R -q -e "library('knitr'); knitr::knit('$base.Rnw')"
+        pdflatex "$base".tex
+        biber "$base"
+        pdflatex "$base".tex
+        pdflatex "$base".tex
+    else
+        echo "no compatible latex program, please install latexmk or pdflatex and biber"
+    fi
 fi
