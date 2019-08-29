b60279b0a6de2da28f9680315437094cf8ef0612
Sid Reed <sidreed34@gmail.com>
2019-08-29 18:44:05 -0400

    moved to i3lock-color, looks much better imo

--- .scripts/lock.sh
+++ .scripts/lock.sh
@@ -1,40 +1,52 @@
 #!/bin/bash
 
-# Take /tmp/screen.pnga screenshot:
-#colors
-insidecolor=00000000
+screensaverpath="/tmp/screen.png"
+#colors,text,font
 ringcolor=ffffffff
-keyhlcolor=d23c3dff
+keyhlcolor=3cc908ff
 bshlcolor=d23c3dff
 separatorcolor=00000000
+insidecolor=00000000
 insidevercolor=00000000
-insidewrongcolor=d23c3dff
 ringvercolor=ffffffff
-ringwrongcolor=ffffffff
+insidewrongcolor=00000000
+ringwrongcolor=ff0000ff
+wrongcolor=ffffffff
 verifcolor=ffffffff
 timecolor=ffffffff
 datecolor=ffffffff
 loginbox=00000066
+datestring=
 font='Terminus:style=Bold'
-locktext='Type password to unlock...'
-screensaverpath="/tmp/screen.png"
-scrot "$screensaverpath" && convert "$screensaverpath" -scale 20% convert -scale 500% "$screensaverpath"
+locktext="$(date +'%A, %B %d %Y' )"
+#drawing rectangles
+rectangles=" "
+SR=$(xrandr --query | grep ' connected' | grep -o '[0-9][0-9]*x[0-9][0-9]*[^ ]*')
+for RES in $SR; do
+    SRA=(${RES//[x+]/ })
+    CX=$((${SRA[2]} + 35))
+    CY=$((${SRA[1]} - 40))
+    rectangles+="rectangle $CX,$CY $((CX+300)),$((CY-80)) "
+done
+
+scrot "$screensaverpath" && convert "$screensaverpath" -scale 20% convert -scale 500% -draw "fill black fill-opacity 0.8 $rectangles" "$screensaverpath"
+mpc pause
 #lock command
 /home/sidreed/apps/i3lock-color/build/i3lock \
     -i "$screensaverpath" \
-    --timepos='x+110:h-70' \
-    --datepos='x+43:h-45' \
-    --clock --date-align 1 --datestr "$locktext" \
+    --timepos='x+115:h-80' \
+    --datepos='x+130:h-60' \
+    --indpos='x+290:h-80' \
+    --clock --datestr "$locktext" \
     --insidecolor=$insidecolor --ringcolor=$ringcolor --line-uses-inside \
-    --keyhlcolor=$keyhlcolor --bshlcolor=$bshlcolor \
-    --separatorcolor=$separatorcolor \
+    --keyhlcolor=$keyhlcolor --bshlcolor=$bshlcolor --separatorcolor=$separatorcolor \
     --insidevercolor=$insidevercolor --insidewrongcolor=$insidewrongcolor \
     --ringvercolor=$ringvercolor --ringwrongcolor=$ringwrongcolor \
-    --indpos='x+280:h-70' \
-    --radius=20 --ring-width=4 --veriftext='' --wrongtext='' \
+    --radius=20 --ring-width=4 --veriftext='' \
+    --wrongtext='' --wrongcolor="$wrongcolor" \
     --verifcolor="$verifcolor" --timecolor="$timecolor" \
     --datecolor="$datecolor" \
     --time-font="$font" --date-font="$font" --layout-font="$font" \
     --verif-font="$font" --wrong-font="$font" \
-    --noinputtext='' --force-clock
+    --noinputtext='' #--force-clock
 
