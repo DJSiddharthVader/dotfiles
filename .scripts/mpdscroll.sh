8a16e7a6df4b2248fce9b78c6965bd11f46a5fbc
Sid Reed <sidreed34@gmail.com>
2019-08-29 18:50:32 -0400

    removed icons from printing, simplified scrolling text

--- .scripts/mpdscroll.sh
+++ .scripts/mpdscroll.sh
@@ -1,23 +1,19 @@
 #!/bin/sh
-#Script to scroll song name of current mpd song in polybar
+#Script to scroll song name of current mpd song in polybar
+spacechar="."
 thresh=20
 if ! mpc >/dev/null 2>&1; then
     echo Server offline
     exit 1
 elif mpc status | grep -q playing; then
     if [ "$(mpc current | wc -c)" -gt "$thresh" ]; then
-        ( zscroll -l "$thresh" -d 0.30 -b "" -a "" -p " /// " -u true "mpc current"  | sed -e :a -ue "s/^.\{1,$thresh\}$/& /;ta" | sed -uEn 's/(^.*$)/ \1/p' ) &
+        ( zscroll -l "$thresh" -d 0.60 -b "" -a "" -p "///" "$(mpc current | sed -uEn 's/(^.*$)/ \1 /p' | tr '[:blank:]' "$spacechar" )" ) &
     else
-        (echo "$(mpc current)" | sed -uEn 's/(^.*$)/ \1 /p') &
+        echo "$(mpc current)"
     fi
 elif mpc status | grep -q paused; then
-    echo " Paused "
+    echo "Paused"
 else
-    echo " Stopped "
+    echo ""
 fi
-
 mpc idle >/dev/null
-#    #( zscroll -l 20 -d 0.30 -b "" -a "" -p " /// " -u true "mpc current"  use zscroll to scroll the output of mpc current with /// as the delim between the end of text
-#    #| sed -e :a -ue 's/^.\{1,20\}$/& /;ta'#pad with space to be 20 chars if not
-#    #| sed -uEn 's/(^.*$)/───| \1|───/p' surround text in ───| |─── for asthetic
-#    #) & capture, run in background
