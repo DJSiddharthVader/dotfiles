27ae1fd4afcb0e044cd5226a13b7d38a79efed8e
Sid Reed <sidreed34@gmail.com>
2019-08-29 18:51:21 -0400

    rewrote to fix some bugs, now can take a range of inputs and print all events

--- .scripts/sched.py
+++ .scripts/sched.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 import sys
 import datetime
-import numpy as np
 import pandas as pd
 
 mindayidx = 3
@@ -12,12 +11,48 @@ schedule = pd.read_csv('~/.scripts/schedules/schedule.cln',delimiter=':')
 def scrapedate():
     now = datetime.datetime.now()
     day =  now.strftime("%a")
-    hour = now.hour
+    hour = int(now.hour)
     if now.minute < 30:
         minn = 00
     else:
         minn = 30
     return day,hour,minn
+def cyclemin(minn,modtime):
+    if (modtime % 2) != 0:
+        if minn == 30:
+            return 0
+        elif minn == 0:
+            return 30
+    else:
+        return minn
+
+def cyclehour(hour,modtime,minn):
+    dayincrement = 0
+    if modtime > 0:
+        if modtime%2 == 0:
+            hour += modtime/2
+        else:
+            if minn == 30:
+                hour += (modtime-1)/2 + 1
+            else:
+                hour += (modtime-1)/2
+    elif modtime < 0:
+        modtime = abs(modtime)
+        if modtime%2 == 0:
+            hour -= modtime/2
+        else:
+            if minn == 30:
+                hour -= (modtime-1)/2 + 1
+            else:
+                hour -= (modtime-1)/2
+    #handle day change
+    if hour >= 24:
+        hour = hour - 24
+        dayincrement = 1
+    elif hour < 0:
+        hour = 24 + hour
+        dayincrement = -1
+    return int(hour),dayincrement
 
 def cycleday(cday,dayincrement):
     if dayincrement == 0:
@@ -30,37 +65,12 @@ def cycleday(cday,dayincrement):
             newidx = 9
         return idxday[newidx]
 
-def cyclemin(minn,modtime):
-    if (modtime % 2) != 0:
-        if minn == 30:
-            return 0
-        elif minn == 0:
-            return 30
+def getactivity(day,hour,minn):
+    activity = schedule[(schedule['hour'] == hour) & (schedule['min'] == minn)][day].values[0]
+    if activity == None:
+        return 'None'
     else:
-        return minn
-
-def cyclehour(hour,modtime,minn):
-    dayincrement=0
-    if modtime > 0:
-        if minn == 30:
-            hour += ((modtime-1)/2)+1
-        elif minn == 0:
-            hour += ((modtime)/2)
-    elif modtime < 0:
-        if minn == 0:
-            hour -= (1+((abs(modtime)-1)/2))
-        elif minn == 30:
-            hour -= abs(modtime)/2
-    else:
-        return hour,dayincrement
-    #handle day change
-    if hour >= 24:
-        hour = hour - 24
-        dayincrement = 1
-    elif hour < 0:
-        hour = 24 + hour
-        dayincrement = -1
-    return hour,dayincrement
+        return activity
 
 def formathour(tfhour,minn):
     am='AM'
@@ -75,25 +85,22 @@ def formathour(tfhour,minn):
         minn = '00'
     return '{}:{}{}'.format(tfhour,minn,am)
 
-def getactivity(day,hour,minn):
-    activity = schedule[(schedule['hour'] == hour) & (schedule['min'] == minn)][day].values[0]
-    if activity == None:
-        return 'None'
-    else:
-        return activity
-
 def main(modtime):
     day,hour,minn = scrapedate()
-    hour,incr = cyclehour(hour,modtime,minn)
-    day = cycleday(day,incr)
-    minn = cyclemin(minn,modtime)
-    activity = getactivity(day,hour,minn)
-    ftime = formathour(hour,minn)
+    gmin = cyclemin(minn,modtime)
+    ghour,incr = cyclehour(hour,modtime,minn)
+    gday = cycleday(day,incr)
+    activity = getactivity(gday,ghour,gmin)
+    ftime = formathour(ghour,gmin)
     return '{} {}'.format(ftime,activity)
 
 
 if __name__ == '__main__':
-    modtime = 0
-    if len(sys.argv) > 1:
-        modtime = int(float(sys.argv[1]))
-    print(main(modtime))
+    modtimes = [0]
+    if len(sys.argv) == 2:
+        modtimes = [int(sys.argv[1])]
+    elif len(sys.argv) == 3:
+        modtimes = list(range(int(sys.argv[1]),int(sys.argv[2])+1))
+    activities = [main(x) for x in modtimes]
+    print('|'.join(activities))
+
