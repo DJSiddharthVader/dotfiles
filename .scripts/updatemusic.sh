d2a773172f1db79ff9f0fb5d6a62474cc5b4a162
Sid Reed <sidreed34@gmail.com>
2019-08-29 18:48:39 -0400

    changed paths to use $HOME

--- .scripts/updatemusic.sh
+++ .scripts/updatemusic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-scdl --download-archive /home/sidreed/dotfiles/.config/soundcloud_download_archive.txt -c -l https://soundcloud.com/sid-reed-871359466/sets/likedsongs
+scdl --download-archive "$HOME/dotfiles/.varfiles/soundcloud_download_archive.txt" -c -l https://soundcloud.com/sid-reed-871359466/sets/likedsongs
 youtube-dl  --audio-quality 0 \
             --audio-format mp3 \
             --prefer-ffmpeg \
@@ -7,6 +7,6 @@ youtube-dl  --audio-quality 0 \
             --yes-playlist \
             -i \
             --verbose \
-            --download-archive /home/sidreed/dotfiles/.config/youtube-dl_downloads.txt \
+            --download-archive "$HOME/dotfiles/.varfiles/youtube-dl_downloads.txt" \
             "https://www.youtube.com/playlist?list=PLMU-V2Iwwq69O1QA3kOtw_YTQ2q-sD-gT"
             #"https://www.youtube.com/playlist?list=PLMU-V2Iwwq699kP0gmoOXyyk02eH4ZgEH"
