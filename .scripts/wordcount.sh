92375a4a6d3a2536d6619c3624879f79605ad637
Sid Reed <sidreed34@gmail.com>
2019-12-31 22:18:31 +0530

    script to count words in my latex coverletter for grad school sops

---
+++ .scripts/wordcount.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+    echo "usage: bash wordcount.sh yourfile.tex"
+else
+    #words="$(detex $1 | grep -v "block" | grep -v "blank" | grep -v "draw" | grep -v "node" | grep -v "d[SIRW]" | wc -w)"
+    #echo "Document has $words words"
+    words="$(sed -n '/begin{cvletter/,/end{cvletter/p' "$1" | grep -v '%' | wc -w)"
+    echo "$words"
+fi
