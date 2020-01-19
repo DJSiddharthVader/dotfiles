6c2d359fbe39bac71147892f6f47f785a5ff0b43
Sid Reed <sidreed34@gmail.com>
2020-01-19 15:12:14 -0500

    display express vpn location, click to toggle connection

---
+++ .config/polybar/scripts/expressvpn.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+function displayStatus() {
+    status="$(expressvpn status)"
+    case $status in
+        'Not Connected')
+            output="N/A"
+            ;;
+        *)
+            output="$(expressvpn status | head -1 | cut -d'-' -f2)"
+            ;;
+    esac
+    echo "$output"
+}
+function toggleConnection() {
+    status="$(expressvpn status)"
+    if [[ "$status" == "Not connected" ]]
+    then
+        expressvpn connect
+    else
+        expressvpn disconnect
+    fi
+}
+function main() {
+    mode="$1"
+    case $mode in
+        'status')
+            displayStatus
+            ;;
+        'toggle')
+            toggleConnection
+            ;;
+        'connect')
+            expressvpn connect
+            ;;
+        'disconnect')
+            expressvpn disconnect
+            ;;
+        *)
+            echo "Usage $0 {status|toggle|connect|disconnect}"
+            exit 1
+            ;;
+    esac
+}
+
+main "$1"
