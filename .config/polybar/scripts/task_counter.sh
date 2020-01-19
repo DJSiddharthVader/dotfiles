edc1abc01ac7836ee2613c659180f9284f32a8cd
Sid Reed <sidreed34@gmail.com>
2020-01-19 15:10:00 -0500

    added toggle between short(total tasks) and long (tasks per category) when clicking on polybar module

--- .config/polybar/scripts/task_counter.sh
+++ .config/polybar/scripts/task_counter.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 task_file="$HOME/Personal/notes/tasks/tasks"
+mode_file="$HOME/dotfiles/.varfiles/taskmode"
 #Task Icons
 #task_icon="Tasks"
 #task_icon=""
@@ -16,13 +17,64 @@ task_icon=""
 task_seperator="  "
 
 
-task_categories=$(cut -d':' -f1 "$task_file" | sort | uniq -c | sort -n)
-total=$(wc -l "$task_file" | cut -d" " -f1)
-out=" $task_icon $total$task_seperator"
-while read -r task_count; do
-    catagory=$(echo "$task_count" | cut -d' ' -f2)
-    count=$(echo "$task_count" | cut -d' ' -f1)
-    out+="$catagory:$count$task_seperator"
-done <<< "$task_categories"
-echo "${out::-2} $task_icon"
+function short() {
+    total=$(wc -l "$task_file" | cut -d" " -f1)
+    out="$task_icon $total"
+    echo $out
+}
+function long() {
+    task_categories=$(cut -d':' -f1 "$task_file" | sort | uniq -c | sort -rn )
+    total=$(wc -l "$task_file" | cut -d" " -f1)
+    out="$task_icon $total"
+    while read -r task_count; do
+        catagory=$(echo "$task_count" | cut -d' ' -f2)
+        count=$(echo "$task_count" | cut -d' ' -f1)
+        out+="$task_seperator $catagory:$count"
+    done <<< "$task_categories"
+    echo "$out"
+}
+function listTasks(){
+    mode="$1"
+    case $mode in
+        'short')
+            short
+            ;;
+        'long')
+            long
+            ;;
+        *)
+            echo "Usage $0 {short|long}"
+            exit 1
+            ;;
+    esac
+}
+function main() {
+    mode="$1"
+    case $mode in
+        'short')
+            echo 'short' >| $mode_file
+            ;;
+        'long')
+            echo 'long' >| $mode_file
+            ;;
+        'toggle')
+            mode="$(cat $mode_file)"
+            if [[ $mode == "short" ]]; then
+                echo 'long' >| $mode_file
+            else
+                echo 'short' >| $mode_file
+            fi
+            ;;
+        '')
+            sleep 0.001
+            ;;
+        *)
+            echo "Usage $0 {toggle|short|long}"
+            exit 1
+            ;;
+    esac
+    mode="$(cat $mode_file)"
+    listTasks $mode
+}
 
+main "$1"
