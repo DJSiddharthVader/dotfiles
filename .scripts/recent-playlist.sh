62f1435f5cfa88c9c4e7b13f061579dbe2638351
Sid Reed <sidreed34@gmail.com>
2020-02-19 23:58:35 -0500

    creates mpd playlist of most recently added songs

---
+++ .scripts/recent-playlist.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+music_dir="$HOME/Music/songs/everything"
+
+#$size most recent songs
+playlist="$HOME/.mpd/playlists/mostrecent.m3u"
+size=50
+find $music_dir -maxdepth 99 -type f -printf "%T+\t%p\n" | sort -n | cut -f2- | tail -"$size" >| $playlist
+
+#all songs modified within mtime minutes (1 week)
+playlist="$HOME/.mpd/playlists/mostrecentadded.m3u"
+mtime=$((60*24*7))
+find $music_dir -maxdepth 99 -type f -mmin -$mtime | cut -f2-  >| $playlist
