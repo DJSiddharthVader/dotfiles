e64aaa257db1509bffcc5d26a2afae76573f0fdb
Sid Reed <sidreed34@gmail.com>
2020-08-29 19:27:07 -0700

    break wordcounting command over lines

--- .scripts/word-count.sh
+++ .scripts/word-count.sh
@@ -4,6 +4,10 @@ if [ $# -eq 0 ]; then
 else
     #words="$(detex $1 | grep -v "block" | grep -v "blank" | grep -v "draw" | grep -v "node" | grep -v "d[SIRW]" | wc -w)"
     #echo "Document has $words words"
-    words="$(sed -n '/begin{cvletter/,/end{cvletter/p' "$1" | grep -v '%' | wc -cw)"
+    #words="$(sed -n '/begin{cvletter/,/end{cvletter/p' "$1" | grep -v '%' | wc -cw)"
+    words="$(sed -n '/begin{document/,/end{document/p' "$1" | \
+             #grep -ov '\\[a-zA-Z0-9]*' | \
+             grep -v '%' | \
+             wc -cw)"
     echo "$words"
 fi
