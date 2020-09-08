a2a50fef324bd1a780c742969e5f4d12a42c9586
Sid Reed <sidreed34@gmail.com>
2020-09-08 15:58:42 -0700

    added .gitignore

--- .scripts/commit-status.sh
+++ .scripts/commit-status.sh
@@ -8,4 +8,5 @@ git commit -m "update vim folds, spellfiles"
 git add .varfiles/*
 git commit -m "update background tracking files"
 git commit .config/qt5ct/colors/pywal.conf -m "update QT5 colors as set by pywal"
+git commit .gitignore -m "update ignore files"
 cd -
