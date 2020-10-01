7aa91b73918c3ee3eef56c912b0757509f82191c
Sid Reed <sidreed34@gmail.com>
2020-10-01 14:16:19 -0700

    update vim folds, spellfiles

--- .scripts/current-wallpaper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wallidfile="$HOME/dotfiles/.varfiles/wallidx"
-wallpfile="$HOME/dotfiles/.varfiles/fehbg"
-picdir="$HOME/Pictures/wallpapers"
-name="$(head -"$(cat $wallidfile)" "$wallpfile" | tail -1)"
-echo "$picdir/$name"
