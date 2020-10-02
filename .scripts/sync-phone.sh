11456c9ef2badefa88e3a5e1629f38bac5b0f709
Sid Reed <sidreed34@gmail.com>
2020-10-02 11:24:07 -0700

    "fixing" sync notes, moved common rsync args into an array, check if phone is connected first, added help function, added arg handling and some flags

--- .scripts/sync-phone.sh
+++ .scripts/sync-phone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Dirs
-music_local="$HOME/Music/songs/everything"
+music_local="$HOME/Music/songs"
 music_phone="~/SDCard/Music/Songs"
 notes_local="$HOME/Documents/Notes"
 notes_phone="~/SDCard/Notes"
@@ -10,42 +10,56 @@ read_phone="~/SDCard/Reading"
 pic_local="$HOME/Pictures/phone"
 pic_phone="~/SDCard/{DCIM,Pictures}"
 
-function sync_notes() {
-    main_branch="laptop"
-    phone_branch="phone"
+#Defaults args
+ip='10.0.0.229'
+port=2222
+notes_only='false'
+rsync_args=("-r" "--human-readable" "--info=progress2")
+
+
+function help(){
+    echo "Usage: $0 \
+            -i \$ip    #ip of target phone   \
+            -p \$port  #port on target phone \
+            -n #only sync notes to save time \
+         "
+    exit 1
+}
+function check_phone_open(){
     ip="$1"
     port="$2"
+    nc -z "$ip" "$port"
+    if [ "$?" = 1 ] ;then
+        echo "Phone connection refused, exiting..."
+        exit 1
+    fi
+}
+function sync_notes() {
+    main_branch="laptop"
+    remote_branch="phone"
+    ip="$1"
+    port="$2"
+    rsync -rq --delete $notes_local/ ~/Archive/Notes/Local/
+    rsync -e "ssh -p $port" --exclude=".*" "${rsync_args[@]}" "$ip:$notes_phone"/ ~/Archive/Notes/Phone/
     # Update local changes
     cd $notes_local
-    commit_msg="local changes on $(date)"
-    git add -A
-    git commit -am "$commit_msg" #commit local changes
+    git checkout $main_branch #switch to tmp branch
+    git add -A && git commit -vam "local changes"
     # Copy phone files in new branch
-    git checkout -b $phone_branch #switch to tmp branch
-    rsync --exclude="^.*"    \
-          -e "ssh -p $port" \
-          -r                \
-          --human-readable  \
-          --info=progress2  \
-          "$ip:$notes_phone" ./
+    git checkout $remote_branch #switch to tmp branch
+    rsync -e "ssh -p $port" --exclude=".*" "${rsync_args[@]}" "$ip:$notes_phone"/ ./
     # Resolve merge
-    git merge $main_branch
-    vim -p "$(git diff --name-only | uniq)"
-    git add -A
-    commit_msg="merge changes on $(date)"
-    git commit -am "$commit_msg" #commit post merge changes
+    git add -A && git commit -vam "remote changes"
+    git checkout $main_branch #switch to tmp branch
+    git merge --no-commit -v $remote_branch
+    diffs="$(git diff --name-only | uniq | tr '\n' ' ')"
+    [[ ! -z "$diffs" ]] && vim -p $diffs
+    git add -A && git commit -vam "merged changes"
     #Sync to phone
-    rsync --exclude=".*"    \
-          -e "ssh -p $port" \
-          -r                \
-          --human-readable  \
-          --info=progress2  \
-          --delete          \
-          ./ "$ip:$notes_phone/"
-    git checkout "$main_branch"
+    rsync -e "ssh -p $port" --exclude=".*" --delete "${rsync_args[@]}" ./ "$ip:$notes_phone"/
     cd -
 }
-function syncdir() {
+function sync_dir() {
     ip="$1"
     port="$2"
     source="$3"
@@ -53,69 +67,50 @@ function syncdir() {
     mode="$5"
     case "$mode" in
         'merge')
-            rsync -e "ssh -p $port" \
-                  -r                \
-                  -u                \
-                  --human-readable  \
-                  --info=progress2  \
-                  $ip:$dest/ $source/
-            rsync -e "ssh -p $port" \
-                  -r                \
-                  -u                \
-                  --human-readable  \
-                  --info=progress2  \
-                  $source/ $ip:$dest/
+            rsync -e "ssh -p $port" "${rsync_args[@]}" -u $ip:$dest/ $source/
+            rsync -e "ssh -p $port"  "${rsync_args[@]}" -u $source/ $ip:$dest/
             ;;
         'update')
-            rsync -e "ssh -p $port"     \
-                  -r                    \
-                  --human-readable      \
-                  --info=progress2      \
-                  $source/ $ip:$dest/
-            ;;
+            rsync -e "ssh -p $port" "${rsync_args[@]}" -u $source/ $ip:$dest/ ;;
         'source')
-            rsync -e "ssh -p $port"     \
-                  -r                    \
-                  --human-readable      \
-                  --info=progress2      \
-                  --delete              \
-                  $source/ $ip:$dest/
-            ;;
+            rsync -e "ssh -p $port" "${rsync_args[@]}" --delete  $source/ $ip:$dest/ ;;
         'dest')
-            rsync -e "ssh -p $port"     \
-                  -r                    \
-                  --human-readable      \
-                  --info=progress2      \
-                  --delete              \
-                  $ip:$dest/ $source/
-            ;;
+            rsync -e "ssh -p $port" "${rsync_args[@]}" --delete $ip:$dest/ $source/ ;;
         *)
-            echo 'invalid mode, use {merge|update|source|dest}'
-            ;;
+            echo 'invalid mode, use {merge|update|source|dest}' ;;
     esac
 }
-function main() {
+function sync_all() {
     ip="$1"
     port="$2"
     echo Music
-    syncdir $ip $port $music_local $music_phone source
+    sync_dir $ip $port $music_local $music_phone source
     echo Reading
-    syncdir $ip $port $read_local $read_phone source
+    sync_dir $ip $port $read_local $read_phone source
     echo Pictures
-    syncdir $ip $port $pic_local $pic_phone merge
+    sync_dir $ip $port $pic_local $pic_phone merge
     echo Notes
-    rsync -qr --delete $notes_local/ "$notes_local"_Backup/
     sync_notes $ip $port
-    #syncdir $ip $port $notes_local/ $notes_phone merge
+    #sync_dir $ip $port $notes_local/ $notes_phone merge
+}
+function main() {
+    #Args
+    while getopts 'i:np:' flag; do
+        case "${flag}" in
+            i) ip="${OPTARG}" ;;
+            p) port="${OPTARG}" ;;
+            n) notes_only='true' ;;
+            *) help ;;
+        esac
+    done
+    check_phone_open "$ip" "$port"
+    #rsync_args=("${rsync_args[@]}" "-e \"ssh -p $port\"")
+    rsync -e "ssh -p $port" "${rsync_args[@]}" "$ip:$notes_phone"/ ./
+    if [ "$notes_only" = true ]; then
+        sync_notes "$ip" "$port"
+    else
+        sync_all "$ip" "$port"
+    fi
 }
 
-#Args
-if [[ $# -eq 0 ]];then
-    ip='10.0.0.229'
-    port=2222
-else
-    ip="$1"
-    port="$2"
-fi
-#sync_notes "$ip" "$port"
-main "$ip" "$port"
+main "$@"
