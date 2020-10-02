c296adaead9ecdbaeaebfcdd48313aedce383985
Sid Reed <sidreed34@gmail.com>
2020-10-02 11:25:32 -0700

    moved into functions

--- .scripts/music_utils/recent-playlist.sh
+++ .scripts/music_utils/recent-playlist.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
-
-music_dir="$HOME/Music/songs/everything"
-
-#$size most recent songs
-playlist="$HOME/.mpd/playlists/mostrecent.m3u"
+source "$HOME/dotfiles/.scripts/music_utils/variables.sh"
 size=50
-find $music_dir -maxdepth 99 -type f -printf "%T+\t%p\n" | sort -n | cut -f2- | tail -"$size" >| $playlist
+mtime=$((60*24*7)) #1 week in seconds
 
-#all songs modified within mtime minutes (1 week)
-playlist="$HOME/.mpd/playlists/mostrecentadded.m3u"
-if [[ $# > 0 ]];then
-    mtime="$1"
-else
-    mtime=$((60*24*7)) #1 week in seconds
-fi
-find $music_dir -maxdepth 99 -type f -mmin -$mtime | cut -f2-  >| $playlist
+function TopMostRecentModified(){
+    #x most recent songs
+    playlist="$1"
+    size="$2"
+    find $music_dir -maxdepth 99 -type f -printf "%T+\t%p\n" | sort -n | cut -f2- | tail -"$size" >| $playlist
+}
+function ModifiedInPastTime(){
+    #all songs modified in the past mtime minutes (1 week)
+    playlist="$1"
+    mtime="$2"
+    find $music_dir -maxdepth 99 -type f -mmin -$mtime | cut -f2-  >| $playlist
+}
+function main(){
+    playlist="$playlist_dir/mostrecent.m3u"
+    TopMostRecentModified $playlist $size
+    playlist="$playlist_dir/mostrecentadded.m3u"
+    ModifiedInPastTime $playlist $mtime
+}
+
+main
