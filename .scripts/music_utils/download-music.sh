711fcb147cae85de18678057ceee0df31c2c6e55
Sid Reed <sidreed34@gmail.com>
2021-02-01 23:54:27 -0800

    wrapper for youtube-dl

--- .scripts/music_utils/download-music.sh
+++ .scripts/music_utils/download-music.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 source ~/dotfiles/.scripts/music_utils/variables.sh
-function updateMusic() {
-    sudo youtube-dl -U
+
+function getStartIndex(){
+    playlist_length="$(youtube-dl "$source_playlist" -J --flat-playlist | jq ".entries | length")"
+    downloaded="$(wc -l "$download_archive" | sed -E 's/^([0-9]+)[^0-9].*$/\1/')"
+    start_index=$(( $playlist_length - $downloaded + 2 )) #incase aborted last time +2 so it doesnt skip a song
+    echo "$start_index"
+}
+function downloadMusic(){
+    #sudo youtube-dl -U
     cd $music_dir
-    youtube-dl  --audio-quality 0                      \
+    #start_index="$(getStartIndex)"
+    #echo "Downloading: $start_index songs"
+    youtube-dl  -ix                                    \
+                --audio-quality 0                      \
                 --audio-format "$format"               \
                 --prefer-ffmpeg                        \
-                --geo-bypass                           \
-                -x                                     \
-                -i                                     \
                 --yes-playlist                         \
                 --playlist-reverse                     \
-                --verbose                              \
-                --max-downloads 5                      \
                 --download-archive "$download_archive" \
                 -o "$youtubedl_format_str"             \
                 "$source_playlist"
     cd -
 }
-updateMusic
+
+downloadMusic
