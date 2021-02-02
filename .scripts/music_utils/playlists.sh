b2e80e6ad1d35ae0b65290b98c0e6e78f2223fdc
Sid Reed <sidreed34@gmail.com>
2021-02-01 23:55:41 -0800

    idk

--- .scripts/music_utils/playlists.sh
+++ .scripts/music_utils/playlists.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source "$HOME/dotfiles/.scripts/music_utils/variables.sh"
 size=50
-mtime=$((60*24*7)) #1 week in seconds
+mtime=$((60*24*7)) #1 week in minutes
 
 function TopMostRecentModified(){
     #x most recent songs
@@ -15,11 +15,26 @@ function ModifiedInPastTime(){
     mtime="$2"
     find $music_dir -maxdepth 99 -type f -mmin -$mtime | cut -f2-  >| $playlist
 }
+function Unorganized(){
+    #all songs modified in the past mtime minutes (1 week)
+    playlist="$1"
+    find $music_dir -maxdepth 1 -type f >| $playlist
+}
+function WholeLibrary(){
+    #all songs in music dir
+    playlist="$1"
+    find $music_dir -maxdepth 99 -type f >| $playlist
+}
+function Genres(){
+    #all songs in each genre
+    playlist="$1"
+    find $music_dir -maxdepth 99 -type f | cut -f2-  >| $playlist
+}
 function main(){
-    playlist="$playlist_dir/mostrecent.m3u"
-    TopMostRecentModified $playlist $size
-    playlist="$playlist_dir/mostrecentadded.m3u"
-    ModifiedInPastTime $playlist $mtime
+    TopMostRecentModified "$playlist_dir/mostrecent.m3u" $size
+    ModifiedInPastTime "$playlist_dir/mostrecentadded.m3u" $mtime
+    WholeLibrary "$playlist_dir/all.m3u"
+    Unorganized "$playlist_dir/unorganized.m3u"
 }
 
 main
--- .scripts/music_utils/variables.sh
+++ .scripts/music_utils/variables.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-music_dir="$HOME/Music/songs/"
-playlist_dir="$HOME/.mpd/playlists/mostrecent.m3u"
+script_dir="$HOME/dotfiles/.scripts/music_utils"
+music_dir="$HOME/Music/songs"
+playlist_dir="$HOME/.mpd/playlists"
 art_dir="$HOME/Pictures/albumart"
-tmp_dir="$HOME/Music/tmp"
 format="mp3"
 field_delimiter="__"
 
 #For update-music
 download_archive="$HOME/dotfiles/.varfiles/youtube-dl_downloads.txt"
 source_playlist="https://www.youtube.com/playlist?list=PLMU-V2Iwwq69O1QA3kOtw_YTQ2q-sD-gT"
-#youtubedl_format_str="./%(track)s$field_delimiter%(artist)s$field_delimiter%(album)s$field_delimiter%(genre)s$field_delimiter%(track_number)s$field_delimiter%(release_year)s$field_delimiter%(title)s$field_delimiter%(playlist_index)s$field_delimiter%(id)s.$format"
-youtubedl_format_str="./%(title)s$field_delimiter%(playlist_index)s$field_delimiter%(id)s.$format"
-
-#For organize-music
-declare -A substitutionPatterns=( ["$"]="S" ["&"]="and" ["/"]="-" )
-declare -A fieldNames=( ["Artist"]="Preformer" ["Title"]="Track Name" ["Album"]="Album" )
+youtubedl_format_str="./%(title)s$field_delimiter%(playlist_index)s$field_delimiter%(id)s.%(ext)s"
 
 
+function progress() {
+    local w=60 p=$1;  shift
+    # create a string of spaces, then change them to dots
+    printf -v dots "%*s" "$(( $p*$w/100 ))" ""; dots=${dots// /#};
+    # print those dots on a fixed-width space plus the percentage etc.
+    printf "\r\e[K|%-*s| %3d %% %s" "$w" "$dots" "$p" "$*";
+}
