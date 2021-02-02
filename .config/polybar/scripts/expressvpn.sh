841045ad9e93bd42dd5ab7057bacaef51900b603
Sid Reed <sidreed34@gmail.com>
2021-02-01 23:12:11 -0800

    fixed update function

--- .config/polybar/scripts/expressvpn.sh
+++ .config/polybar/scripts/expressvpn.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+downloadpage='https://www.expressvpn.com/latest?utm_source=linux_app#linux'
+
 function update() {
-    downloadpage='https://www.expressvpn.com/latest?utm_source=linux_app#linux'
-    deb_local='~/Downloads/latest_expressvpn.deb'
     if [ "$(expressvpn status | grep -c 'A new version is available')" -eq 1 ]; then
         deb_url="$(wget -O - "$downloadpage" | grep 'name=\"linux-download\"' | grep -oP '(?<=value=\")https.*(?=\">Ubuntu)')"
-        wget "$deb_url" -O "$deb_local"
-        sudo dpkg -i $deb_local
-        \rm -f $deb_local
+        cd ~/Downloads && wget "$deb_url"
+        sudo dpkg -i expressvpn*.deb
+        rm -f expressvpn*.deb && cd -
         echo "Finished"
     else
         echo "No update available"
@@ -15,19 +15,15 @@ function update() {
     fi
 }
 function displayStatus() {
-    status="$(expressvpn status)"
+    status="$(expressvpn status | tail -1)"
     case $status in
-        'Not Connected')
-            output="N/A"
-            ;;
-        *)
-            output="$(expressvpn status | grep 'onnect' | head -1 | cut -d'-' -f2 | tr -d '\[\;[0-9]m?' )"
-            ;;
+        'Not Connected') output="N/A" ;;
+        *) output="$(expressvpn status | grep 'onnect' | head -1 | cut -d'-' -f2 | tr -d '\[\;[0-9]m?' )" ;;
     esac
     echo "$output "
 }
 function toggleConnection() {
-    status="$(expressvpn status)"
+    status="$(expressvpn status | tail -1)"
     if [[ "$status" == "Not connected" ]]
     then
         expressvpn connect
