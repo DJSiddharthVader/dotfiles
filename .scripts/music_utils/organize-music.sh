2695274b47e826c90434f1596bd3d1c46917e3a2
Sid Reed <sidreed34@gmail.com>
2021-02-01 23:54:47 -0800

    organizes music files based on metadata

--- .scripts/music_utils/organize-music.sh
+++ .scripts/music_utils/organize-music.sh
@@ -1,77 +1,84 @@
 #!/bin/bash
-source ~/dotfiles/.scripts/music_utils/variables.sh
 ##TODO:
-# - fetch album from album/artist name for song -> download -> name correctly
-# - get lyrics, write to file
-# - get song metadata
-#    - necessary are:
-#      - title
-#      - album
-#      - artist
-#    - would be nice:
-#      - genre
-#      - year
-#      - track number
-#      - Date
-#      - Comment (youtube download ID)
 # - output plots of song data
 #    - genre distributions
 #    - length distributions
 #    - common phrases/words in titles/albums/arists/lyrics
+source ~/dotfiles/.scripts/music_utils/variables.sh
+declare -A char_replacements=( ["&"]=+ [/]=_ [" "]=. ) #[""]=""
 
-function progress() {
-    local w=60 p=$1;  shift
-    # create a string of spaces, then change them to dots
-    printf -v dots "%*s" "$(( $p*$w/100 ))" ""; dots=${dots// /#};
-    # print those dots on a fixed-width space plus the percentage etc.
-    printf "\r\e[K|%-*s| %3d %% %s" "$w" "$dots" "$p" "$*";
+function usage(){
+    echo "
+Usage $0 { all | \$FILES }
+
+   all:    organizes all files in $music_dir
+   \$FILES: renames each file provided as an arg
+" && exit 0
 }
-function addMetadataToFile() {
-    songfile="$1" #file with name specified by $youtubedl_format_str
-    image="$2"
-    lame --quiet --add-id3v2 --tl "$album" --ta "$artist" --tt "$title"               "$file" "$file"
-    if [[ "$album" =~ 'Single' ]]; then #if no album art
-        lame --quiet --ti "$image" "$file" "$file".keep
+function subCharacters(){
+    #substitues characters that may exist in a metadata field but are bad for filenames
+    if (( $# == 0 )); then
+        str="$(cat)"
+    else
+        str="$1"
     fi
+    str="$(echo "$str" | sed -E 's/ (.) /\1/g')"
+    for pattern in "${!char_replacements[@]}"; do
+        str="$(echo $str | tr "$pattern" "${char_replacements[$pattern]}")"
+    done
+    echo "$str"
 }
-function getSongField() {
+function getSongField(){
     #for a song file get the metadat field value of the specified field
-    file="$1"
+    file="$(readlink -e "$1")"
     field="$2"
     mediainfo "$file" | grep "$field" | tail -1 | cut -d':' -f2- | \
     sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//'
 }
-function substituteInvalidCharacters() {
-    #substitues characters that may exist in a metadata field but are bad for filenames
-    # file can have all ASCII Characters
-    str="$1"
-    for pattern in "${!MYMAP[@]}"; do
-        str="$(echo $str | tr "$pattern" "${substitutionPatterns[$pattern]}")"
-    done
-    echo "$str"
+function makeFileName(){
+    filename="$1"
+    song="$(  getSongField "$filename" 'Track name' | subCharacters)"
+    artist="$(getSongField "$filename" 'Performer'  | subCharacters)"
+    album="$( getSongField "$filename" 'Album'      | subCharacters)"
+    suffix="$(basename "$filename" | perl -pe 's/^(.+?)__(.+)$/\2/')"
+    newname="$artist/$album/$song$field_delimiter$artist$field_delimiter$suffix"
+    echo "$newname"
 }
-function makeFileName() {
-    songfile="$1"
-    titleformat="$2"
-    extension="$(echo $songfile | rev | cut -d'.' -f1 | rev )"
-    newfile=""
-    for field in ${titleformat//$field_delimiter/$'\n'}; do
-        toadd="$(getSongField $songfile $field)"
-        newfile="$newfile$field_delimiter$toadd"
-    done
-    newfile="$(substituteInvalidCharacters $newfile)"
-    echo "$newfile.$extension"
+function renameSongFile(){
+    file="$(readlink -e "$1")"
+    newfilename="$(makeFileName "$file")"
+    newdir="$music_dir/$(dirname "$newfilename")"
+    mkdir -p "$newdir"
+    mv "$file" "$music_dir/$newfilename"
+    echo "$music_dir/$newfilename"
 }
-function organizeMusicFiles() {
-    total="$(find "$music_dir" -type f | wc -l)"
+function organizeMusic(){
     counter=0
+    total="$(find "$music_dir" -maxdepth 1 -type f | wc -l)"
     echo "Organizing mp3s, $total files"
     while IFS='' read -r -d '' file; do
-        renameSongFile "$file" 2> /dev/null
+        renameSongFile "$file" > /dev/null
         percent=$(printf "%.*f" 0 "$(echo "100*$counter/$total" | bc -l)")
         progress $percent
         let counter++
-    done < <(find -L "$music_dir" -type f -name '*.mp3' -print0)
+    done < <(find -L "$music_dir" -maxdepth 1 -type f -name '*.mp3' -print0)
     find "$music_dir" -maxdepth 99 -type d -empty -delete #delete empty directories
+    echo ''
 }
+function reOrganizeMusic(){
+    find "$music_dir" -maxdepth 99 -type f -name '*.mp3' -exec mv {} "$music_dir" \;
+    organizeMusic
+}
+
+if (( $# < 1 )); then
+    usage
+elif [[ "$1" == 'all' ]]; then
+    organizeMusic
+elif [[ "$1" == 'redo' ]]; then
+    reOrganizeMusic
+else
+    for file in "$@"; do
+        renameSongFile "$file"
+    done
+fi
 
