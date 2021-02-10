a0073643da2b2b3ff7b9cc846e3ca01a8ce3a3df
Sid Reed <sidreed34@gmail.com>
2021-02-09 23:52:50 -0800

    trimmed it down alot and simplified the cycling behaviour to match polyar modules code

--- .scripts/bar-manager.sh
+++ .scripts/bar-manager.sh
@@ -1,97 +1,72 @@
 #!/bin/bash
+shopt -s extglob
 
-togglefile="$HOME/dotfiles/.varfiles/bartoggle"
-declare -A modes_idxs
-modes_idxs=([float]=1 [full]=2 [mini]=3 [none]=4)
-modes_fwd=('padding' 'float' 'full' 'mini' 'none')
-modes_rev=('padding' 'mini' 'none' 'float' 'full')
 bgfile="$HOME/dotfiles/.varfiles/bordered_background.png"
 unbgfile="$HOME/dotfiles/.varfiles/unbordered_background.png"
+mode_file="$HOME/dotfiles/.varfiles/barmode"
+modes=(float full mini none)
 
-function launchBar() {
+help() { echo "Error: usage ./$(basename $0) {reload|restart|next|prev|$(echo ${modes[*]} | tr ' ' '|')}" ; }
+cycle() {
+    # cycle through modes either forwards or backwards
+    # get index of current mode in the modes array, find index for next/previous mode and get the array value of that index and echo it
+    # next mode index is:  (x+1) % n
+    # prev mode index is:  (x+n-1) % n
+    # x is current mode index, n is number of modes
+    dir="$1"
+    mode="$(cat $mode_file)"
+    idx="$(echo "${modes[*]}" | grep -o "^.*$mode" | tr ' ' '\n' | wc -l)"
+    idx=$(($idx -1)) #current mode idx
+    case "$dir" in
+         'next') idx=$(($idx + 1)) ;;
+         'prev') idx=$(($idx +${#modes[@]} -1)) ;;
+         *) echo "Error cycle takes {next|prev}" && exit 1 ;;
+    esac
+    next_idx=$(($idx % ${#modes[@]})) #modulo to wrap back
+    echo "${modes[$next_idx]}"
+}
+wallpaper() {
+    echo "$1"
+    case $1 in
+        float|mini|none) feh --bg-scale "$unbgfile" ;;
+        full) feh --bg-scale "$bgfile" ;;
+        *) help && echo 'wall' && exit 1 ;;
+    esac
+}
+launch() {
     mode="$1"
+    wallpaper "$mode"
     killall -q polybar && sleep 0.00001 # Terminate already running bar instances
     for m in $(xrandr | grep ' connected' | cut -d' ' -f1); do
         case "$mode" in
-            float)
+            'float')
                 MONITOR=$m polybar floating_top &
                 MONITOR=$m polybar floating_bot &
                 ;;
-            full)
+            'full' )
                 MONITOR=$m polybar bordered_top &
                 MONITOR=$m polybar bordered_bot &
                 ;;
-            mini)
-                MONITOR=$m polybar minimal &
-                ;;
-            none)
-                sleep 1
-                ;;
-            *)
-                echo "usage $0 {float|full|mini|none}"
-                exit 1
-                ;;
+            'mini' ) MONITOR=$m polybar minimal & ;;
+            'none' ) sleep 1 ;;
+            *) help && echo 'launch' && exit 1 ;;
         esac
     done
 }
-function setWallPaper() {
-    case "$1" in
-        float)
-            feh --bg-scale "$unbgfile"
-            ;;
-        full)
-            feh --bg-scale "$bgfile"
-            ;;
-        mini)
-            feh --bg-scale "$unbgfile"
-            ;;
-        none)
-            feh --bg-scale "$unbgfile"
-            ;;
-        *)
-            echo "usage $0 {float|full|mini|none}"
-            exit 1
-            ;;
-    esac
-}
-function writeToggleFile() {
+main() {
     mode="$1"
-    nextidx="$(( ${modes_idxs[$mode]} % ${#modes_idxs[@]} + 1 ))"
-    fmode="${modes_fwd[$nextidx]}"
-    rmode="${modes_rev[$nextidx]}"
-    echo -e "$mode\n$fmode\n$rmode" >| "$togglefile"
-}
-function updateBar() {
-    mode="$1"
-    launchBar "$mode"
-    setWallPaper "$mode"
-    writeToggleFile "$mode"
-}
-function barWrapper() {
-    option="$1"
-    case "$option" in
-        next) mode="$(head -2 $togglefile | tail -1)" ;;
-        prev) mode="$(head -3 $togglefile | tail -1)" ;;
-        stay) mode="reload" ;;
-        restart) mode="$(head -1 $togglefile | tail -1)" ;;
-        float) mode="float" ;;
-        full) mode="full" ;;
-        mini) mode="mini" ;;
-        none) mode="none" ;;
-        *) echo "Usage: $0 {next|prev|stay|float|full||mini|none}"
-           exit 1
-          ;;
+    tmp="@($(echo ${modes[*]} | sed -e 's/ /|/g'))"
+    #echo "start: $mode $(cycle prev) $(cat $mode_file) $(cycle next) $tmp"
+    case "$mode" in
+        'reload' ) ! [[ -z "$(pgrep 'polybar')" ]] && polybar-msg cmd restart && exit 0 ;;
+        'restart') dmode="$(cat $mode_file)" ;; #same mode, just launch again
+        'next'   ) dmode="$(cycle 'next')" ;;
+        'prev'   ) dmode="$(cycle 'prev')" ;;
+         $tmp    ) dmode="$mode" ;; #capture any valid mode
+        *) echo "failed: $mode" && help && exit 1 ;;
     esac
-    if [ "$mode" = "reload" ]; then
-        if ! [ -z "$(pgrep 'polybar')" ]; then
-            # launch bar if not already launched
-            polybar-msg cmd restart
-            exit 0 #exit
-        else
-            mode="$(head -1 $togglefile)" # current mode, kill an restart bar
-        fi
-    fi
-    updateBar "$mode"
+    echo "$dmode"  >| "$mode_file"
+    launch "$(cat $mode_file)"
 }
 
-barWrapper "$1"
+main "$1"
