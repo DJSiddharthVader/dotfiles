d4bcb508a55ed9f966fc40657959b998dcba7752
Sid Reed <sidreed34@gmail.com>
2021-02-11 15:42:12 -0800

    idk hope it works

--- .scripts/transmission-start.sh
+++ .scripts/transmission-start.sh
@@ -3,8 +3,4 @@
 download_dir="$HOME/Torrents/"
 torrent_dir="$HOME/.config/transmission-daemon/torrents"
 
-transmission-daemon
-transmission-remote -w "$download_dir"
-for file in "$torrent_dir"/*.torrent; do
-    transmission-remote -a "$file"
-done
+transmission-remote -tall -s
