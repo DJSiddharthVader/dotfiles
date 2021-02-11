143d666cfa7b6bde9b58ce75ac025ab1940d17ab
Sid Reed <sidreed34@gmail.com>
2021-02-11 15:38:33 -0800

    added mode to bring up rofi display to pick country

--- .config/polybar/scripts/expressvpn.sh
+++ .config/polybar/scripts/expressvpn.sh
@@ -10,36 +10,41 @@ update() {
         rm -f expressvpn*.deb && cd -
         echo "Finished"
     else
-        echo "No update available"
-        exit 0
+        echo "No update available" && exit 0
     fi
 }
+status() { expressvpn status | tail -1 ; }
+connect() { expressvpn connect ; }
+disconnect() { expressvpn disconnect ; }
+toggle() { [[ "$(status)" == "Not connected" ]] && connect || disconnect ; }
+pick_location() {
+    location="$(expressvpn list all | tail -n+3  |\
+                sed -e 's/ - /-/g' -e 's/[ Y]\+$//' |\
+                grep -o '  [A-Za-z\-]*[^\s]$' | cut -f3 -d' ' |\
+                rofi -m -3 -width 20 -lines 5 -dmenu \|
+                )"
+    code="$(expressvpn list all | grep "$(echo "$location" | sed -e 's/-/ - /g')" | head -1 | cut -f1 -d' ')"
+    [[ "$(status)" == "Not connected" ]] || disconnect
+    expressvpn connect "$code"
+}
 display() {
-    status="$(expressvpn status | tail -1)"
-    case $status in
+
+    case "$(status)" in
         'Not Connected') output="N/A" ;;
         *) output="$(expressvpn status | grep 'onnect' | head -1 | cut -d'-' -f2 | tr -d '\[\;[0-9]m?' )" ;;
     esac
     echo " $output"
 }
-toggle() {
-    status="$(expressvpn status | tail -1)"
-    if [[ "$status" == "Not connected" ]]
-    then
-        expressvpn connect
-    else
-        expressvpn disconnect
-    fi
-}
 main() {
     mode="$1"
     case $mode in
-        'status') display ;;
-        'toggle') toggle ;;
-        'connect') expressvpn connect ;;
-        'disconnect') expressvpn disconnect ;;
-        'update') update ;;
-        *) echo "Usage $0 {status|toggle|connect|disconnect|update}" && exit 1 ;;
+        'status'    ) display       ;;
+        'pick'      ) pick_location ;;
+        'toggle'    ) toggle        ;;
+        'connect'   ) connect       ;;
+        'disconnect') disconnect    ;;
+        'update'    ) update        ;;
+        *) echo "Usage $0 {status|pick|toggle|connect|disconnect|update}" && exit 1 ;;
     esac
 }
 
