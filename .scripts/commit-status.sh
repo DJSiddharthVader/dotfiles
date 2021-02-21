93e998cb14a59d97a4c53e7a5688d53a999fa431
Sid Reed <sidreed34@gmail.com>
2021-02-20 22:27:24 -0800

    added zathura color updates

--- .scripts/commit-status.sh
+++ .scripts/commit-status.sh
@@ -4,9 +4,10 @@ dotdir="$HOME/dotfiles"
 
 cd $dotdir
 git add .vim/view/* .vim/.netrwhist .vim/spell/*
-git commit -m "update vim folds, spellfiles"
 git add .varfiles/*
+git commit -m "update vim folds, spellfiles"
 git commit -m "update background tracking files"
 git commit .config/qt5ct/colors/pywal.conf -m "update QT5 colors as set by pywal"
+git commit .config/zathura/zathurarc -m "update zathura colors as set by pywal"
 git commit .gitignore -m "update ignore files"
 cd -
