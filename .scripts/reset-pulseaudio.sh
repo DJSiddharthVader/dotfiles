5a821a2f35f915999a538c3b308438579186266b
Sid Reed <sidreed34@gmail.com>
2021-02-20 22:35:36 -0800

    removed a bunch of stuff i was trying and stoped using

--- .scripts/reset-pulseaudio.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mpc pause
-pulseaudio --kill
-pulseaudio --kill
-pulseaudio --kill
-pulseaudio --start
-~/dotfiles/.scripts/bar-manager.sh stay
--- .scripts/reset-wifi.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /etc
-sudo ln -sf /run/resolvconf/resolv.conf
-sudo /etc/init.d/network-manager restart
-cd -
