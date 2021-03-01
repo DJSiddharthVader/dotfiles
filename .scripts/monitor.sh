ee878d5d55c74b9c74fcef33c5a11a5714dcba16
Sid Reed <sidreed34@gmail.com>
2021-03-01 07:29:52 -0800

    re-wrote to loop over all external monitors and connect (clamshell mode)

--- .scripts/monitor.sh
+++ .scripts/monitor.sh
@@ -1,78 +1,47 @@
 #!/bin/bash
+primary="eDP-1"
 
-function get_secondary_resolution() {
+resolution() {
     monitor="$1"
-    resolution="$(xrandr | sed -ne "/$monitor/,/connected/ p" |
-                  grep -v 'connected' |
-                  head -1 |
-                  grep -ao '[0-9]*x[0-9]*')"
-    echo $resolution
+    xrandr | sed -ne "/$monitor/,/connected/ p"  \
+           | grep -v 'connected'  \
+           | head -1  \
+           | grep -ao '[0-9]*x[0-9]*'
 }
-connectToMonitor() {
-    connected=$(xrandr | grep -c ' connected')
-    if [ "$connected" -gt 1 ]; then #connected to a monitor
-        monitor="$(xrandr | grep ' connected' | tail -1 | cut -d' ' -f1)"
-        resolution="$(get_secondary_resolution "$monitor")"
-        #echo "$resolution"
-        #pactl set-card-profile 0 output:hdmi-stereo
-        xrandr --output eDP-1       \
-               --output "$monitor"  \
-               --mode "$resolution" \
-               --right-of eDP-1
-    fi
+connect() {
+    #for monitor in $(xrandr | grep ' connected'); do
+    first=""
+    while IFS= read -r monitor; do
+        if [[ "$monitor" != "$primary" ]]; then
+            resolution="$(resolution "$monitor")"
+            echo $monitor $resolution $first
+            if [[ -z "$first" ]]; then
+                xrandr --output "$monitor" --mode "$resolution" --primary
+                #pactl set-card-profile 0 output:hdmi-stereo
+                first="$monitor"
+            else
+                xrandr --output "$monitor" --mode "$resolution" --right-of "$first"
+            fi
+        fi
+    done <<< "$(xrandr | grep ' connected' | cut -f1 -d' ')"
+    xrandr --output "$primary" --off #turn off main monitor
 }
-disconnectMonitor() {
-    monitor=`xrandr | grep ' connected' | tail -1 | cut -d' ' -f1`
-    xrandr --output "$monitor" --off
-    ishdmi=`xrandr | grep ' connected' | tail -1`
-    if [[ $ishdmi =~ 'HDMI' ]]; then
-        pactl set-card-profile 0 output:hdmi-stereo
-        pactl set-card-profile 0 output:analog-stereo
-    fi
+disconnect() {
+    xrandr --output "$primary" --mode "$(resolution $primary)" --primary
+    while IFS= read -r monitor; do
+        echo "$monitor"
+        if [[ $monitor != $primary ]]; then
+            xrandr --output "$monitor" --off
+        fi
+    done <<< "$(xrandr | grep ' connected' | cut -d' ' -f1)"
 }
 function main() {
     case "$1" in
-        on)
-            connectToMonitor
-            ~/dotfiles/.scripts/bar-manager.sh restart
-            ;;
-        off)
-            disconnectMonitor
-            ~/dotfiles/.scripts/bar-manager.sh restart
-            ;;
-        *)
-            echo "usage {on|off}"
-            exit 1
-            ;;
+        on) connect; ~/dotfiles/.scripts/bar-manager.sh restart ;;
+        off) disconnect; ~/dotfiles/.scripts/bar-manager.sh restart ;;
+        *) echo "usage {on|off}" && exit 1 ;;
     esac
 }
 
 main "$1"
 
-#DEPRECIATED
-#function get_secondary_resolution() {
-#    # get the  largest resolution of the first conncted monitor
-#    ishdmi="$(xrandr | grep ' connected' | tail -1)"
-#    if [[ $ishdmi =~ 'HDMI' ]]; then
-#        #resolution="$(xrandr | grep -v 'primary' | grep -Pzo '.* connected(.*\n)*' | grep -a '^ ' | sort -rn | grep -v '[0-9]i' | grep -v '\*' | grep -v i | head -2 | tail -1 | grep -ao '[0-9]*x[0-9]*')"
-#        resolution="$(xrandr | grep -v 'primary' |
-#                      grep -Pzo '^ .* connected(.*\n)*' |
-#                      grep -a '^ ' |
-#                      sort -rn |
-#                      grep -v '[0-9]i' |
-#                      grep -v '\*' |
-#                      head -2 | tail -1 |
-#                      grep -ao '[0-9]*x[0-9]*')"
-#    else
-#        #resolution="$(xrandr | grep -v 'primary' | grep -Pzo '.* connected(.*\n)*' | grep -a '^ ' | sort -rn | grep -v '[0-9]i'  | grep -v '\*' | head -1 | grep -ao '[0-9]*x[0-9]*')"
-#        resolution="$(xrandr | grep -v 'primary' |
-#                      grep -Pzo '.* connected(.*\n)*' |
-#                      grep -a '^ ' |
-#                      sort -rn |
-#                      grep -v '[0-9]i'  |
-#                      grep -v '\*' |
-#                      head -1 |
-#                      grep -ao '[0-9]*x[0-9]*')"
-#    fi
-#    echo $resolution
-#}
