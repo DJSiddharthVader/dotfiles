25bc8b75c8cb82af65006213f32a339c91ecb14a
Sid Reed <sidreed34@gmail.com>
2021-03-01 07:37:57 -0800

    added functions to search torrents directly from the web using rofi and get the magnet links and download

--- .scripts/polybar/transmission.sh
+++ .scripts/polybar/transmission.sh
@@ -3,10 +3,13 @@ shopt -s extglob
 
 mode_file="$HOME/dotfiles/.varfiles/tormode"
 stats="$HOME/dotfiles/.config/transmission-daemon/stats.json"
+config_dir="$HOME/dotfiles/.config/transmission-daemon"
+watch_dir="$HOME/Torrents"
 modes=(ratio data datatotal speed active) #no spaces in mode titles
+max_pages=5
 
 help() {
-    echo "Error: usage ./$(basename $0) {display|next|prev|$(echo ${modes[*]} | tr ' ' '|')}"
+    echo "Error: usage ./$(basename $0) {search|display|next|prev|$(echo ${modes[*]} | tr ' ' '|')}"
 }
 cycle() {
     # cycle through modes either forwards or backwards
@@ -89,21 +92,77 @@ display() {
     esac
     echo "$msg"
 }
+
+start() {
+    transmission-daemon -g $config_dir -w $watch_dir
+    for torrent in $config_dir/torrents/*.torrent*; do
+        echo "$torrent"
+        transmission-remote -a "$torrent"
+    done
+}
+
+extract_from_page() {
+    grep "$1" "$2" | sed -e 's/^.*>\([^<>]\+\)<\(\/td\|\/a\|span\).*$/\1/'
+}
+parse_results() {
+    page="$1"
+    names="$(extract_from_page 'coll-1 name' $page)"
+    seeders="$(extract_from_page 'coll-2 seeds' $page)"
+    leechers="$(extract_from_page 'coll-3 leeches' $page)"
+    date="$(extract_from_page 'coll-date' $page)"
+    size="$(extract_from_page 'coll-4 size' $page)"
+    paste <(echo "$date") <(echo "$seeders") <(echo "$leechers") <(echo "$size") <(echo "$names") | tail -n+2 | tr -s ' ' | tr -s '\t' #| column -t -s'\t'
+}
+get_search_results() {
+    query="$1"
+    results_page="$2"
+    search_url="https://1337x.to/search/${query// /+}/1/"
+    #curl -Ss "$search_url" >| ~/pages.html
+    total_pages="$(curl -Ss "$search_url" | grep 'Last' |  sed -s 's/^.*class=.last.><a href=.\/search\/.*\/\([0-9]*\)\/.>.*$/\1/')"
+    [[ $max_pages -lt $total_pages ]] && total_pages=$max_pages
+    for page_num in $(seq 1 $total_pages); do
+        search_url="https://1337x.to/sort-search/${query// /+}/seeders/desc/$page_num/"
+        echo $search_url
+        curl -Ss "$search_url" >> $results_page
+    done
+    #cp $results_page ~/results.html
+}
+search() {
+    query="$(rofi -lines 0 -dmenu -p "Enter query")"
+    [[ -z "$query" ]] && exit 1 #no query
+    results_page=$(mktemp)
+    get_search_results "$query" "$results_page"
+    chosen="$(parse_results $results_page | rofi -lines 25 -width 80 -dmenu -p "Pick torrent")"
+    [[ -z "$chosen" ]] && exit 1 #no query
+    txlink="$(grep -F "$(echo "$chosen" | rev | cut -f1 | rev)" $results_page | sed -e 's/^.*href="\/\(torrent.*\)\/">.*$/\1/')"
+    [[ -z "$txlink" ]] && exit 1 #no query
+    magnet="$(curl -Ss "https://1337x.to/$txlink/" | grep 'magnet' | head -1 | sed -e 's/^.*href="\(magnet[^"<>]*\)".*$/\1/')"
+    echo "$magnet"
+    [[ -z "$magnet" ]] && exit 1 #no query
+    transmission-remote -a "$magnet" && notify-send "$(echo $chosen | rev | cut -f1 | rev) torrent added"
+    rm $results_page
+}
+
 main() {
     mode="$1"
-    if [[ "$mode" == 'display' ]]; then
-        [[ -z "$2" ]] && dmode="$(cat $mode_file)" || dmode="$2"
-        display "$dmode"
-    else
-        tmp="@($(echo ${modes[*]} | sed -e 's/ /|/g'))"
-        case "$mode" in
-            'next') dmode="$(cycle 'next')" ;;
-            'prev') dmode="$(cycle 'prev')" ;;
-            $tmp  ) dmode="$mode" ;; #capture any valid mode
-            *) help && exit 1 ;;
-        esac
-        echo "$dmode"  >| "$mode_file"
-    fi
+    case $mode in
+        'search' ) search ;;
+        'start'  ) start ;;
+        'display')
+            [[ -z "$2" ]] && dmode="$(cat $mode_file)" || dmode="$2"
+            display "$dmode"
+            ;;
+        *)
+            tmp="@($(echo ${modes[*]} | sed -e 's/ /|/g'))"
+            case "$mode" in
+                'next'  ) dmode="$(cycle 'next')" ;;
+                'prev'  ) dmode="$(cycle 'prev')" ;;
+                $tmp    ) dmode="$mode" ;; #capture any valid mode
+                *       ) help && exit 1 ;;
+            esac
+            echo "$dmode"  >| "$mode_file"
+            ;;
+    esac
 }
 
 main "$@"
