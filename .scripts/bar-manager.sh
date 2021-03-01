7f659e8b95d3cc6b3a3e73465741695eaa0b27e3
Sid Reed <sidreed34@gmail.com>
2021-03-01 07:28:23 -0800

    cleaned up a little, added function to cycle polybar module separator icons

--- .scripts/bar-manager.sh
+++ .scripts/bar-manager.sh
@@ -3,10 +3,22 @@ shopt -s extglob
 
 bgfile="$HOME/dotfiles/.varfiles/bordered_background.png"
 unbgfile="$HOME/dotfiles/.varfiles/unbordered_background.png"
+
 mode_file="$HOME/dotfiles/.varfiles/barmode"
 modes=(float full mini none)
 
-help() { echo "Error: usage ./$(basename $0) {reload|restart|next|prev|$(echo ${modes[*]} | tr ' ' '|')}" ; }
+icon_file="$HOME/dotfiles/.varfiles/polysep"
+icons=(arrow_sym trig_left trig_right circle arrow_tail trig_out trig_in arrow_curve)
+icon_sets=(       )
+#icons in order for module sepration
+#symetric    ; ; ; ; ; ;
+#directional ;  ;  ; ; ;【】
+
+help() {
+    name=$1[@]
+    arr=("${!name}")
+    echo "Error: usage ./$(basename $0) {style|sep|reload|restart} {stay|next|prev$(echo '|'${arr[*]} | tr ' ' '|')}"
+}
 cycle() {
     # cycle through modes either forwards or backwards
     # get index of current mode in the modes array, find index for next/previous mode and get the array value of that index and echo it
@@ -14,31 +26,63 @@ cycle() {
     # prev mode index is:  (x+n-1) % n
     # x is current mode index, n is number of modes
     dir="$1"
-    mode="$(cat $mode_file)"
-    idx="$(echo "${modes[*]}" | grep -o "^.*$mode" | tr ' ' '\n' | wc -l)"
+    name=$2[@]
+    arr=("${!name}")
+    mode="$(cat $3)"
+    idx="$(echo "${arr[*]}" | grep -o "^.*$mode" | tr ' ' '\n' | wc -l)"
     idx=$(($idx -1)) #current mode idx
     case "$dir" in
          'next') idx=$(($idx + 1)) ;;
-         'prev') idx=$(($idx +${#modes[@]} -1)) ;;
+         'prev') idx=$(($idx +${#arr[@]} -1)) ;;
          *) echo "Error cycle takes {next|prev}" && exit 1 ;;
     esac
-    next_idx=$(($idx % ${#modes[@]})) #modulo to wrap back
-    echo "${modes[$next_idx]}"
+    next_idx=$(($idx % ${#arr[@]})) #modulo to wrap back
+    echo "${arr[$next_idx]}"
 }
 wallpaper() {
-    echo "$1"
     case $1 in
         float|mini|none) feh --bg-scale "$unbgfile" ;;
         full) feh --bg-scale "$bgfile" ;;
-        *) help && echo 'wall' && exit 1 ;;
+        *) help 'style' && echo 'wall' && exit 1 ;;
     esac
+}
+separators() {
+    mode="$1"
+    tmp="@($(echo ${icons[*]} | sed -e 's/ /|/g'))"
+    case "$mode" in
+        'stay') dmode="$(head -1 $icon_file)" ;;
+        'next') dmode="$(cycle 'next' icons $icon_file)" ;;
+        'prev') dmode="$(cycle 'prev' icons $icon_file)" ;;
+         $tmp ) dmode="$mode" ;; #capture any valid mode
+        *) echo "failed: $mode" && help sep && exit 1 ;;
+    esac
+    idx="$(echo "${icons[*]}" | grep -o "^.*$dmode" | tr ' ' '\n' | wc -l | sed -s 's/$/-1/' | bc)"
+    #idx=$(($idx -1)) #current mode idx
+    icon_set="${icon_sets[$idx]}"
+    echo -e "$mode\n$dmode\n$idx\n$icon_set"
+    echo "$dmode
+; icons to delimit polybar modules
+leftprefix =  ${icon_set:0:1}
+leftsuffix =  ${icon_set:1:1}
+rightprefix = ${icon_set:2:1}
+rightsuffix = ${icon_set:3:1}
+" >| "$icon_file"
 }
 launch() {
     mode="$1"
-    wallpaper "$mode"
+    tmp="@($(echo ${modes[*]} | sed -e 's/ /|/g'))"
+    case "$mode" in
+        'stay') dmode="$(cat $mode_file)" ;;
+        'next') dmode="$(cycle 'next' modes $mode_file)" ;;
+        'prev') dmode="$(cycle 'prev' modes $mode_file)" ;;
+         $tmp ) dmode="$mode" ;; #capture any valid mode
+        *) echo "failed: $mode" && help style && exit 1 ;;
+    esac
+    echo "$dmode"  >| "$mode_file"
+    wallpaper "$dmode"
     killall -q polybar && sleep 0.00001 # Terminate already running bar instances
     for m in $(xrandr | grep ' connected' | cut -d' ' -f1); do
-        case "$mode" in
+        case "$dmode" in
             'float')
                 MONITOR=$m polybar floating_top &
                 MONITOR=$m polybar floating_bot &
@@ -55,18 +99,15 @@ launch() {
 }
 main() {
     mode="$1"
-    tmp="@($(echo ${modes[*]} | sed -e 's/ /|/g'))"
-    #echo "start: $mode $(cycle prev) $(cat $mode_file) $(cycle next) $tmp"
-    case "$mode" in
-        'reload' ) ! [[ -z "$(pgrep 'polybar')" ]] && polybar-msg cmd restart && exit 0 ;;
-        'restart') dmode="$(cat $mode_file)" ;; #same mode, just launch again
-        'next'   ) dmode="$(cycle 'next')" ;;
-        'prev'   ) dmode="$(cycle 'prev')" ;;
-         $tmp    ) dmode="$mode" ;; #capture any valid mode
-        *) echo "failed: $mode" && help && exit 1 ;;
+    change="$2"
+    echo "$mode $change"
+    case $mode in
+        'sep'    ) separators "$change"; launch 'stay' ;;
+        'style'  ) launch "$change" ;;
+        'reload' ) ! [[ -z "$(pgrep 'polybar')" ]] && polybar-msg cmd restart ;;
+        'restart') separators "$(head -1 $icon_file)"; launch "$(cat $mode_file)" ;;
+        *) echo "failed: $mode" && help $mode && exit 1 ;;
     esac
-    echo "$dmode"  >| "$mode_file"
-    launch "$(cat $mode_file)"
 }
 
-main "$1"
+main "$@"
