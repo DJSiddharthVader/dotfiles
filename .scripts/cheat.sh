e83600094ab878edcca6cf8d21938bfbe6cb5852
Sid Reed <sidreed34@gmail.com>
2021-03-16 06:12:45 -0700

    add support for vieweing markdown files with pandoc/w3m

--- .scripts/cheat.sh
+++ .scripts/cheat.sh
@@ -8,15 +8,16 @@ pick() {
    sheet="$(find "$cheat_sheet_dir"/*.* -exec basename {} \; |\
             rev | cut -f2- -d'.' | rev |\
             rofi -m -1 -width 20 -lines 10 -dmenu)"
-   find "$cheat_sheet_dir"/*.* -name "*$sheet.*"
+   [[ -z "$sheet" ]] && echo "" || find "$cheat_sheet_dir"/*.* -name "$sheet.*"
 }
 open() {
     sheet="$1"
     [[ -z "$sheet" ]] && echo "no selection" && exit
     ext="${sheet##*.}"
     case "$ext" in
-        'pdf') tabbed -n 'cheatsheet' -g "$pdfgeom" zathura "$sheet" -e ;;
-        'txt') st -n 'cheatsheet' -g "$termgeom" -e nvim -R -u "$vimconfig" "$sheet" ;;
+        'pdf') tabbed -n 'cheatsheet' -g "$pdfgeom"  zathura "$sheet" -e ;;
+        'txt') st     -n 'cheatsheet' -g "$termgeom" -e nvim -R -u "$vimconfig" "$sheet" ;;
+        'md' ) st     -n 'cheatsheet' -g "$termgeom" -e sh -c "pandoc $sheet | w3m -T text/html" ;;
         *) echo "Error: invalid extension in $1" && exit 1 ;;
     esac
     #i3-msg floating enable > /dev/null
