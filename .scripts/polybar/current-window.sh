18c435263367c29bf4a5ecf374c89ebbcc1fe443
Sid Reed <sidreed34@gmail.com>
2021-03-27 08:08:34 -0700

    works now, just issue with icon in polybar, really a polybar font issue

--- .scripts/polybar/current-window.sh
+++ .scripts/polybar/current-window.sh
@@ -1,20 +1,44 @@
 #!/bin/bash
+shopt -s extglob
 
-icon() {
-    echo 'none'
-    case $1 in
-        '') app="" ;;
-        *)  app="" ;;
-    esac
-    echo $app
+#wsicon="" #$(echo -e '\uF260')"
+thresh=40
+
+getIcon() {
+    id="$1"
+    if [ "$(xprop -id $id | grep -c 'GTK_APPLICATION')" -gt 0 ]; then
+        icon=""
+    else
+        class="$(xprop -id $id WM_CLASS | cut -d\" -f4)"
+        name="$(xprop -id $id WM_NAME | cut -d'=' -f2)"
+        case "$class $name" in
+            *st-256color*) icon="" ;;
+            *Firefox*    ) icon="" ;;
+            *zathura*     ) icon="" ;;
+            *) icon="" ;;
+        esac
+    fi
+    echo $icon
+}
+getWorkspace() {
+    id="$1"
+    index="$(xprop -id $id _NET_WM_DESKTOP | cut -d'=' -f2 | tr -d '" ' | sed -s 's/$/+1/' | bc)"
+    wslist="$(i3-msg -t get_tree | grep -Eo '.num.:[0-9][0-9]*' | cut -d':' -f2)"
+    echo "$wslist" | head -"$index" | tail -1
+
+}
+getName() {
+    id="$1"
+    name="$(xprop -id $id WM_NAME | cut -d'=' -f2 | tr -d '"' | tr -s " " | sed -e 's/[Nn]one//')"
+    echo $name
 }
 main() {
-    sleep 5
     id=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
-    ws="$(xprop -id $id | grep '^_NET_WM_DESKTOP' | cut -d'=' -f2 | tr -d '" ' | sed -s 's/$/+1/' | bc)"
-    name="$(xprop -id $id | grep '^WM_NAME' | cut -d'=' -f2 | tr -d '" ')"
-    icon="$(icon $name)"
-    echo "$id $ws $icon $name"
+    ws="$(getWorkspace $id)"
+    icon="$(getIcon $id)"
+    name="$(getName $id)"
+    #left pad until length $thresh
+    echo "$wsicon $ws $icon $name" | cut -c -$thresh
 }
 
 main
