aad165c78c543bdb94ae173888112de134377c46
Sid Reed <sidreed34@gmail.com>
2021-03-28 20:21:15 -0700

    restyled to look more powerlinely

--- .tmux/tmux-prompt.sh
+++ .tmux/tmux-prompt.sh
@@ -14,7 +14,7 @@ set -g status-bg "colour2"
 #set -g status-left-attr "none"
 setw -g window-status-separator ""
 set -g status-left ""
-set -g status-right ' #{?client_prefix,#[bg=colour3]^A ,}#[fg=colour235,bg=colour5] #H | %m/%d | %H:%M '
+set -g status-right '#[fg=colour5,bg=colour2]#{?client_prefix,#[bg=colour3]^A ,}#[fg=colour235,bg=colour5] #H | %m/%d | %H:%M '
 setw -g window-status-format "#[fg=colour0,bg=colour2] #I #[fg=colour0,bg=colour2]#W "
 setw -g window-status-current-format "#[fg=colour2,bg=colour1,nobold,nounderscore,noitalics]#[fg=colour0,bg=colour1] #W #[fg=colour1,bg=colour2,nobold,nounderscore,noitalics]"
 
