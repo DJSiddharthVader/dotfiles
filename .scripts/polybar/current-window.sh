7ebbc3f65e202a1eaad27d39145ed920f12ee143
Sid Reed <sidreed34@gmail.com>
2021-03-28 20:22:57 -0700

    fixed some errors with xprop to handle empty workspace, get workspace number direct from i3

--- .scripts/polybar/current-window.sh
+++ .scripts/polybar/current-window.sh
@@ -2,29 +2,35 @@
 shopt -s extglob
 
 #wsicon="" #$(echo -e '\uF260')"
-thresh=40
+thresh=30
 
 getIcon() {
     id="$1"
-    if [ "$(xprop -id $id | grep -c 'GTK_APPLICATION')" -gt 0 ]; then
+    wininfo="$(xprop -id $id)"
+    if [ "$(echo "$wininfo" | grep '_NET_WM_NAME(UTF8_STRING)')" = "i3" ]; then
+        icon=""
+    elif [ "$(echo "$wininfo" | grep -c 'GTK_APPLICATION')" -gt 0 ]; then
         icon=""
     else
-        class="$(xprop -id $id WM_CLASS | cut -d\" -f4)"
-        name="$(xprop -id $id WM_NAME | cut -d'=' -f2)"
+        class="$(echo "$wininfo" | grep WM_CLASS | cut -d\" -f4)"
+        name="$(echo "$wininfo" | grep WM_NAME | cut -d'=' -f2)"
         case "$class $name" in
             *st-256color*) icon="" ;;
             *Firefox*    ) icon="" ;;
             *.pdf*       ) icon="" ;;
+            *mpv*        ) icon="" ;;
             *) icon="" ;;
         esac
     fi
     echo "$icon"
 }
 getWorkspace() {
-    id="$1"
-    index="$(xprop -id $id _NET_WM_DESKTOP | cut -d'=' -f2 | tr -d '" ' | sed -s 's/$/+1/' | bc)"
-    wslist="$(i3-msg -t get_tree | grep -Eo '.num.:[0-9][0-9]*' | cut -d':' -f2)"
-    echo "$wslist" | head -"$index" | tail -1
+#    id="$1"
+#    index="$(xprop -id $id _NET_WM_DESKTOP | cut -d'=' -f2 | tr -d '" ' | sed -s 's/$/+1/' | bc)"
+#    wslist="$(i3-msg -t get_tree | grep -Eo '.num.:[0-9][0-9]*' | cut -d':' -f2)"
+#    echo "$wslist" | head -"$index" | tail -1
+    # get currently focused i3 workspace
+    i3-msg -t get_workspaces | jq '.[] | select(.focused==true).name' | cut -d"\"" -f2
 
 }
 getName() {
@@ -33,16 +39,22 @@ getName() {
     if [[ "$name" =~ ".pdf" ]]; then
         echo "$name" | rev | cut -d'/' -f-2 | rev | sed -e 's/^/...\//'
     else
-        echo "$name"
+        echo "$name" #| rev | cut -d'-' -f2- | rev
     fi
 }
 main() {
     id=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
-    ws="$(getWorkspace $id)"
-    icon="$(getIcon $id)"
-    name="$(getName $id)"
-    #left pad until length $thresh
-    echo "$wsicon $ws $icon $name" | cut -c -$thresh
+    xprop -id $id > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        icon="$(getIcon $id)"
+        name="$(getName $id)"
+    else
+        icon=""
+        name=""
+    fi
+    ws="$(getWorkspace)" # focused window workspace
+    # trucate output to desired length
+    echo "$ws $icon $name" | cut -c -$thresh
 }
 
 main
