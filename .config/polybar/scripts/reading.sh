1d86dcc881caaaf9213a477a5b3bffa2ad2fee38
Sid Reed <sidreed34@gmail.com>
2021-04-05 13:45:32 -0700

    fixed so it reads modes/articles correcrlt, tried getting ipc to work with zscroll but idk

--- .config/polybar/scripts/reading.sh
+++ .config/polybar/scripts/reading.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-readings="$HOME/dotfiles/.varfiles/readings"
+readings="$HOME/.readings.txt"
 mode_file="$HOME/dotfiles/.config/polybar/modules.mode"
 reading_folder="reading list"
 browser="firefox"
-thresh=50
+thresh=60
 
 help() {
     echo "Error Usage: $0 {update|pick|open|display}"
 }
-getReading() {
-    grep '^reading:' "$mode_file" | cut -d':' -f2-
+getArticle() {
+    grep '^reading:' "$mode_file" | cut -d':' -f2- | cut -d'~' -f1
+}
+getLink() {
+    grep '^reading:' "$mode_file" | cut -d':' -f2- | cut -d'~' -f2
 }
 setReading() {
-    sed -i "/^reading:/s/:.*/:$1/" "$mode_file"
+    escaped=$(printf '%s\n' "$1" | sed -e 's/[\/&]/\\&/g') # link has funny chars, must escape for sed
+    sed -i "/^reading:/s/:.*/:$escaped/" "$mode_file"
 }
 
 update() {
@@ -30,21 +34,23 @@ update() {
                    | tr -s ' ' ' '                    >| $readings
 }
 pick() {
-    rnd_article="$(cat $readings | shuf | tail -1)"
-    setReading "$rnd_article"
+    setReading "$(cat $readings | shuf | tail -1)"
     killall -q "$(basename $0)" > /dev/null 2>&1 #restart zscroll with new article so scroll updates
+    #polybar-msg hook reading 1 # for ipc messaging to polybar
 }
 open() {
-    "$browser" --new-tab "$(cat $reading | cut -f2 -d'~')" #open article in browser
+    "$browser" --new-tab "$(getLink)" #open article in browser
 }
 display() {
     #scroll article title if longer than $thresh characeters otherwise static display
-    articlename="$(getReading | cut -f1 -d'~')"
+    articlename="$(getArticle)"
     if [[ ${#articlename} -gt $thresh ]]; then
-        ( zscroll -l "$thresh" -d 0.20 -b "" -a "" -p " /// " "$articlename" ) &
-       wait
+        ( zscroll -l "$thresh" -d 0.20 -b "" -a "" -p " /// " "$(getArticle)" ) &
+        #[( zscroll -l "$thresh" -d 0.20 -b "" -a "" -p " /// " "$(grep '^reading:' "$mode_file" | cut -d':' -f2- | cut -d'~' -f1)" ) &
+        wait
     else
-        echo "$articlename"
+        # echo "$articlename"
+        getArticle
     fi
 }
 
