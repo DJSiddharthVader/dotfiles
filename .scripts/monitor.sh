243e47cb8a403a71cb4d994412aa33ca9623deb7
Sid Reed <sidreed34@gmail.com>
2021-06-06 22:11:15 -0700

    re-wrote the whole thing, now works effectively for external monitor setups

--- .scripts/monitor.sh
+++ .scripts/monitor.sh
@@ -1,47 +1,69 @@
 #!/bin/bash
-primary="eDP-1"
+laptop_screen="eDP-1"
 
+help() {
+    echo "Usage: $0 {multi|hybrid|laptop}"
+}
 resolution() {
     monitor="$1"
-    xrandr | sed -ne "/$monitor/,/connected/ p"  \
-           | grep -v 'connected'  \
-           | head -1  \
-           | grep -ao '[0-9]*x[0-9]*'
+    xrandr | sed -ne "/$monitor/,/connected/p" | head -2 | tail -1 | grep -ao '[0-9]*x[0-9]*'
+}
+listMonitors() {
+    xrandr | grep ' connected' | cut -d' ' -f1 | tr -d ' '
+}
+disconnect(){
+    # disconnect all external monitors except laptop screen
+    while IFS= read -r monitor; do
+        if [[ $monitor != $laptop_screen ]]; then
+            echo "disconnecting $monitor"
+            xrandr --output $monitor --off
+        fi
+    done <<< "$(listMonitors)"
+}
+connectToAll() {
+    primary="$1"
+    prev=""
+    while IFS= read -r monitor; do
+        resolution="$(resolution "$monitor")"
+        if [[ -z "$prev" ]]; then
+            if [[ -z "$primary" ]]; then
+                xrandr --output $monitor --mode "$resolution" --primary
+            else
+                xrandr --output $monitor --mode "$resolution" --right-of $primary
+            fi
+        else
+            xrandr --output $monitor --mode "$resolution" --right-of $prev
+        fi
+        prev=$monitor
+    done <<< "$(listMonitors)"
 }
 connect() {
-    #for monitor in $(xrandr | grep ' connected'); do
-    first=""
-    while IFS= read -r monitor; do
-        if [[ "$monitor" != "$primary" ]]; then
-            resolution="$(resolution "$monitor")"
-            echo $monitor $resolution $first
-            if [[ -z "$first" ]]; then
-                xrandr --output "$monitor" --mode "$resolution" --primary
-                #pactl set-card-profile 0 output:hdmi-stereo
-                first="$monitor"
-            else
-                xrandr --output "$monitor" --mode "$resolution" --right-of "$first"
-            fi
-        fi
-    done <<< "$(xrandr | grep ' connected' | cut -f1 -d' ')"
-    xrandr --output "$primary" --off #turn off main monitor
-}
-disconnect() {
-    xrandr --output "$primary" --mode "$(resolution $primary)" --primary
-    while IFS= read -r monitor; do
-        echo "$monitor"
-        if [[ $monitor != $primary ]]; then
-            xrandr --output "$monitor" --off
-        fi
-    done <<< "$(xrandr | grep ' connected' | cut -d' ' -f1)"
+    setup="$1"
+    case "$setup" in
+        'hybrid')
+            connectToAll "$laptop_screen"
+            ;;
+        'multi')
+            connectToAll ""
+            xrandr --output "$laptop_screen" --off
+            pacmd set-card-profile 0 output:hdmi-stereo
+            ;;
+        'laptop')
+            xrandr --output "$laptop_screen" --mode "$(resolution $laptop_screen)" --primary
+            disconnect
+            pacmd set-card-profile 0 output:analog-stereo+input:analog-stereo
+            ;;
+        *) help && exit 1 ;;
+    esac
 }
 function main() {
+    connect "$1"
     case "$1" in
-        on) connect; ~/dotfiles/.scripts/bar-manager.sh restart ;;
-        off) disconnect; ~/dotfiles/.scripts/bar-manager.sh restart ;;
-        *) echo "usage {on|off}" && exit 1 ;;
+        multi) ~/dotfiles/.scripts/bar-manager.sh style float ;;
+        hybrid) ~/dotfiles/.scripts/bar-manager.sh style float ;;
+        laptop) ~/dotfiles/.scripts/bar-manager.sh style laptop ;;
+        *) help && exit 1 ;;
     esac
 }
 
 main "$1"
-
