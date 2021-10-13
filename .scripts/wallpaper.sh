d356108912343f89f50a9520b1de962d4d6c63f5
Siddharth Reed <slreed@andrew.cmu.edu>
2021-10-13 14:30:02 -0400

    change pdf first

--- .scripts/wallpaper.sh
+++ .scripts/wallpaper.sh
@@ -119,10 +119,10 @@ colorFirefox() {
 changeColors() {
     image="$1"
     wal -geni "$image"  # use wal to generate colorschemes from image
+    ~/dotfiles/.scripts/zathura.sh # re-write zathura config with new colors
     ~/dotfiles/.scripts/bar-manager.sh reload > /dev/null 2>&1 # reload polybar with new colors
     colorFirefox # trigger reloading of colors.css in firefox
     ~/Apps/oomox-gtk-theme/change_color.sh -o pywal ~/.cache/wal/colors.oomox # theme for GTK apps and whatnot
-    ~/dotfiles/.scripts/zathura.sh # re-write zathura config with new colors
     timeout 0.1s xsettingsd -c ~/.varfiles/gtkautoreload.ini # live reload all GTK app colors
     # this must be run last since the timeout blocks anything after it from executing due to the set -e (script exists on error, including timeout)
     # if no timeout it would run infinitely and script would never finish
