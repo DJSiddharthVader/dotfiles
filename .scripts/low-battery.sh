124abccda6feaa462f1248b58468b24e119cddea
Siddharth Reed <slreed@andrew.cmu.edu>
2021-11-14 17:37:35 -0400

    completly re-wrote to be cleaner, more flexible. Also bug fixes so it works now

--- .scripts/low-battery.sh
+++ .scripts/low-battery.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
+threshes=(10 15 20 25)
+
 send_notif() {
     msg="$1"
     notify-send "$msg" -u critical
 }
-
-threshes=(10 15 20 25)
-while true; do
-    status="$(acpi -b | grep -o " [^ ]*Charging" | head -1)"
-    if [[ "$status" = " Discharging" ]]; then
-        power=$(acpi -b | cut -d' ' -f4 | cut -d'%' -f1 | head -1)
-        for thresh in ${threshes[@]}; do
-            if (( $power < $thresh )) ; then
-                send_notif "Battery < $thresh%, Charge laptop"
-                break
-            fi
-        done
-    fi
-    sleep 30
-done
+main() {
+    while true; do
+        status="$(acpi -b | grep -o " [^ ]*harging" | head -1)"
+        if [[ "$status" = " Discharging" ]]; then
+            power=$(acpi -b | grep -v "unavailable" | cut -d' ' -f4 | cut -d'%' -f1 | head -1)
+            for thresh in ${threshes[@]}; do
+                if (( $power < $thresh )) ; then
+                    send_notif "Battery < $thresh%"
+                    break
+                fi
+            done
+        fi
+        sleep 30
+    done
+}
+main
