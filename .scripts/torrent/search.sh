6110971ee237d38758afc0cbadae6360ebd2e3c3
Siddharth Reed <slreed@andrew.cmu.edu>
2021-12-13 04:16:37 -0500

    moved the actual parsing of results into its own script, search does less by itself now

--- .scripts/torrent/search.sh
+++ .scripts/torrent/search.sh
@@ -3,91 +3,33 @@ shopt -s extglob
 
 # search 1337x.to for torrents given a query and echo the magnet links as one space delimited string
 
-max_pages=5 # max number of results pages to parse when searching
+MAX_PAGES=5 # max number of results pages to parse when searching
 
-get_1337x_results() {
-    # download the search results for a torrent query from 1337x.to
-    query="$1"
-    results_page="$2"
-    search_url="https://1337x.to/search/${query// /+}/1/"
-    total_pages="$(curl -Ss "$search_url" | grep 'Last' | sed -s 's/^.*class=.last.><a href=.\/search\/.*\/\([0-9]*\)\/.>.*$/\1/')"
-    [[ $max_pages -lt $total_pages ]] && total_pages=$max_pages
-    for page_num in $(seq 1 $total_pages); do
-        search_url="https://1337x.to/sort-search/${query// /+}/seeders/desc/$page_num/"
-        #echo $search_url
-        curl -Ss "$search_url" >> "$results_page"
-    done
-}
-get_kickass_results() {
-    # download the search results for a torrent query from 1337x.to
-    query="$1"
-    results_page="$2"
-    search_url="https://katcr.to/usearch/${query// /+}/1/"
-    total_pages="$(curl -Ss "$search_url" | grep 'Last' |  sed -s 's/^.*class=.last.><a href=.\/search\/.*\/\([0-9]*\)\/.>.*$/\1/')"
-    [[ $max_pages -lt $total_pages ]] && total_pages=$max_pages
-    for page_num in $(seq 1 $total_pages); do
-        search_url="https://1337x.to/sort-search/${query// /+}/seeders/desc/$page_num/"
-        #echo $search_url
-        curl -Ss "$search_url" >> "$results_page"
-    done
-}
-extract_from_page() {
-    # extract information about each torrent from the downloaded html results
-    grep "$2" "$1" | sed -e "s/^.*>\([^<>]\+\)<$3.*$/\1/"
-    # $1 is field, $2 is results page, $3 is the ending tag
-    # ^.*  # from start
-    # >\([^<>]\+\)< # capture everything between ><
-    # \(\/td\|\/a\|span\) # patterns that signal end of info
-    # .*$ # rest of the line matched
-}
-parse_results() {
-    # parse downloaded torrent results into a tab delimited table for display
-    page="$1"
-    sed -i '/<\/th>/d' "$page" # filter crap
-    names="$(extract_from_page $page 'coll-1 name' '\/a')"
-    seeders="$(extract_from_page $page 'coll-2 seeds' '\/td')"
-    leechers="$(extract_from_page $page 'coll-3 leeches' '\/td')"
-    dates="$(extract_from_page $page 'coll-date' '\/td')"
-    sizes="$(extract_from_page $page 'coll-4 size' 'span')"
-   # echo "$names" | tr '\t' '\n' | wc -l 1>&2
-    paste <(echo "$dates") <(echo "$seeders") <(echo "$leechers") <(echo "$sizes") <(echo "$names")
-}
 search() {
     # download search results for query
     query="$(rofi -lines 0 -dmenu -p "Enter Torrent Query")"
     [[ -z "$query" ]] && exit 1 #no query
-    results_page=$(mktemp)
-    get_1337x_results "$query" "$results_page"
+    results=$(mktemp)
+    ~/.scripts/torrent/search_1337x.sh "$query" "$MAX_PAGES"  >| "$results" 
+    ~/.scripts/torrent/get_kickass_results.sh "$query" "$MAX_PAGES" >> "$results"
+    formatted_results=$(mktemp)
+    paste <(seq 1 "$(wc -l "$results" | cut -d' ' -f1)") $results |\
+            tr -s ' ' >| "$formatted_results"
     # pick result(s)
-    chosen="$(parse_results $results_page |\
-              tail -n+2 | tr -s ' ' |\
-              sed 's/\t/=|=/g' | column -s'=' -t |\
+    chosen="$(cut -f1,3- "$formatted_results" | sed 's/\t/=|=/g' | column -s'=' -t |\
               rofi -lines 25 -width 80 -dmenu -multi-select -p "Pick Torrent")"
-    [[ -z "$chosen" ]] && exit 1 #no query
-    chosen_results="$(mktemp)"
-    echo "$chosen" | rev | cut -d'|' -f1 | rev | sed 's/^ *\([^ ].*\) *$/\1/' >| "$chosen_results"
-    # parse magnet link from chosen result and echo
-    links="$(grep -F -f "$chosen_results" "$results_page" | sed -e 's/^.*href="\/\(torrent.*\)\/">.*$/\1/')"
-    links_added=0
-    magnets=""
-    while read link; do
-        # add magnet link and count
-        magnet="$(curl -Ss "https://1337x.to/$link/" | grep 'magnet' | head -1 | sed -e 's/^.*href="\(magnet[^"<>]*\)".*$/\1/')"
-        if [[ -n "$magnet" ]]; then
-            links_added=$((links_added + 1))
-            magnets="$magnets $magnet"
-        fi
-    done <<< "$(echo $links | tr ' ' '\n')"
-    echo "$magnets"
+    [[ -z "$chosen" ]] && echo "no queries selected, exiting" && exit 0 #no query
+    ids="$(echo "$chosen" | cut -d'|' -f1 | sed 's/^\([^ ]*\).*$/^\1\t/')"
+    grep -f <(echo "$ids") "$formatted_results" | cut -f1,2  # print all magnet lints to stdout
     # send notification
-    total="$(wc -l $chosen_results | cut -d' ' -f1)"
+    total="$(wc -l $formatted_results | cut -d' ' -f1)"
     if [[ $total -eq 1 ]]; then
         name="$(head -1 "$chosen_results" | rev | cut -f1 | rev | tr -s ' ' '.')"
         notify-send "Found: $name"
     else
-        notify-send "Query: $query, Links Found: $links_added/$total"
+        links_added="$(wc -l $chosen_results | cut -d' ' -f1)"
+        notify-send "Query: $query, Links added: $links_added"
     fi
-    rm $results_page $chosen_results
 }
 
 search
---
+++ .scripts/torrent/search_1337x.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+set -euo pipefail
+shopt -s extglob
+
+help() {
+    echo "Usage: ./$(basename $0) query max_pages"
+}
+get_results() {
+    # download the search results for a torrent query from 1337x.to
+    query="${1:-}"
+    max_pages="${2:-}"
+    search_url="https://1337x.to/search/${query// /+}/1/"
+    total_pages="$(curl -Ss "$search_url" | grep 'Last' | sed -s 's/^.*class=.last.><a href=.\/search\/.*\/\([0-9]*\)\/.>.*$/\1/')"
+    [[ $max_pages -lt $total_pages ]] && total_pages=$max_pages
+    for page_num in $(seq 1 $total_pages); do
+        search_url="https://1337x.to/sort-search/${query// /+}/seeders/desc/$page_num/"
+        #echo $search_url
+        curl -Ss "$search_url" 
+    done
+}
+extract_from_page() {
+    # extract information about each torrent from the downloaded html results
+    grep "${2:-}" "${1:-}" | sed -e "s/^.*>\([^<>]\+\)<${3:-}.*$/\1/"
+    # ${$1:-} is field, $2 is results page, $3 is the ending tag
+    # ^.*  # from start
+    # >\([^<>]\+\)< # capture everything between ><
+    # \(\/td\|\/a\|span\) # patterns that signal end of info
+    # .*$ # rest of the line matched
+}
+get_links(){
+    # parse magnet link from chosen result and echo
+    links="$(grep '/torrent/' "${results}" | sed -e 's/^.*href="\/\(torrent.*\)\/">.*$/\1/')"
+    magnets=""
+    while read link; do
+        magnet="$(curl -Ss "https://1337x.to/$link/" |\
+                  grep 'magnet' | head -1 |\
+                  sed -e 's/^.*href="\(magnet[^"<>]*\)".*$/\1/')"
+        [[ -n "$magnet" ]] && magnets="$magnets$magnet\n"
+    done <<< "$(echo $links | tr ' ' '\n')"
+    echo -e "$magnets" 
+}
+parse_results() {
+    # parse downloaded torrent results into a tab delimited table for display
+    page="${1:-}"
+    sed -i '/<\/th>/d' "$page" # filter crap
+    links="$(get_links "$page")"
+    echo "$links" 1>&2 
+    names="$(extract_from_page $page 'coll-1 name' '\/a')"
+    seeders="$(extract_from_page $page 'coll-2 seeds' '\/td')"
+    leechers="$(extract_from_page $page 'coll-3 leeches' '\/td')"
+    dates="$(extract_from_page $page 'coll-date' '\/td')"
+    sizes="$(extract_from_page $page 'coll-4 size' 'span')"
+    paste <(echo "$links") <(echo "$dates") <(echo "$seeders") <(echo "$leechers") <(echo "$sizes") <(echo "$names")
+    # paste <(echo "$dates") <(echo "$seeders") <(echo "$leechers") <(echo "$sizes") <(echo "$names") 
+}
+main() {
+    query="${1:-}"
+    max_pages="${2:-}"
+    # results="$(mktemp)"
+    results="results.html"
+    get_results "$query" "$max_pages" >| "$results"
+
+
+    parse_results "$results" | grep -vi 'xxx' # filter porn
+}
+
+[[ $# -eq 2 ]] || (help  && exit 1)
+query="${1:-}"
+max_pages="${2:-}"
+main "$query" "$max_pages"
