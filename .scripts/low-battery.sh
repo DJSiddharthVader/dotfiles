3bb8c6b89d378a93fc3811546240d44a950ecc50
Siddharth Reed <slreed@andrew.cmu.edu>
2021-12-22 01:37:01 -0500

    fixed bug so it detects when charging

--- .scripts/low-battery.sh
+++ .scripts/low-battery.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-
 threshes=(10 15 20 25)
-
 send_notif() {
     msg="$1"
     notify-send "$msg" -u critical
 }
+get_status() {
+    acpi -b | grep -v 'unavailable' | sed -e 's/^.* \([^ ]*harging\).*$/\1/'
+}
+get_percent() {
+    acpi -b | grep -v 'unavailable' | sed -e 's/^.* \([0-9]*\)%.*$/\1/'
+}
 main() {
     while true; do
-        status="$(acpi -b | grep -o " [^ ]*harging" | head -1)"
-        if [[ "$status" = " Discharging" ]]; then
-            power=$(acpi -b | grep -v "unavailable" | cut -d' ' -f4 | cut -d'%' -f1 | head -1)
+        if [[ "$(get_status)" == "Discharging" ]]; then
             for thresh in ${threshes[@]}; do
-                if (( $power < $thresh )) ; then
+                if (( $(get_percent) < $thresh )) ; then
                     send_notif "Battery < $thresh%"
                     break
                 fi
@@ -21,4 +23,5 @@ main() {
         sleep 30
     done
 }
+
 main
