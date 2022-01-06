e1078aa03009c6b19393605b05b685f99cdf39f7
Siddharth Reed <slreed@andrew.cmu.edu>
2022-01-06 16:49:05 -0500

    cleaned up names and fixed small bug

--- .scripts/backup.sh
+++ .scripts/backup.sh
@@ -2,30 +2,29 @@
 home="/home/sidreed"
 backup_drive="/media/4tbdrive"
 rsync_cmd="rsync -a --ignore-existing --info=progress2"
-sync=(Apps CMU_MSCB Music Pictures)
-update=(Games Documents Projects Reading ToOrganize Torrent)
-ignore=(dotfiles)
+to_sync=(Apps CMU_MSCB Music Pictures)
+to_update=(Games Documents Projects Reading ToOrganize Torrent)
+to_ignore=(dotfiles)
 
 
 help() {
     echo "Usage: ./$(basename $0) {backup|sync|update|all}"
 }
-sync_dir() {
-    for dir in ${dirs_to_sync[@]}; do
+sync() {
+    for dir in ${to_sync[@]}; do
         echo "Syncing $dir..."
         $rsync_cmd --delete "$home/$dir" "$backup_drive"
     done
 }
-update_dir() {
-    for dir in ${dirs_to_update[@]}; do
+update() {
+    for dir in ${to_update[@]}; do
         echo "Updating $dir..."
         $rsync_cmd "$home/$dir" "$backup_drive"
     done
 }
-backup_home() {
-    exclude_flags="$(echo ${sync[@]} ${update[@]} ${ignore[@]} \
-                     | sed -e "s:[^ ]*:--exclude=${home}\/&:g")"    
-    total_size="$(du -sb $exclude_flags "$home" | awk '{print $1}')"
+backup() {
+    exclude_flags="$(echo ${to_sync[@]} ${to_update[@]} ${to_ignore[@]} | sed -e "s:[^ ]*:--exclude=${home}\/&:g")"    
+    total_size="$(du -sb $exclude_flags "$home" | awk '{print $1}' 2> /dev/null)"
     tar -pcf - $exclude_flags "$home" \
         | pv -s $total_size \
         | gzip > "$backup_drive/Backups/homedirE560_$(date +'%Y-%m-%d-%H-%M').tar.gz"
@@ -33,13 +32,10 @@ backup_home() {
 main(){
     mode="$1"
     case "$mode" in
-        'backup') backup_home ;;
-        'sync')   sync_dir    ;;
-        'update') update_dir  ;;
-        'all')    backup_home
-                  sync_dir
-                  update_dir
-        ;;
+        'backup') backup ;;
+        'sync'  ) sync   ;;
+        'update') update ;;
+        'all')    backup && sync && update ;;
         *) help && exit 1;;
     esac
 }
