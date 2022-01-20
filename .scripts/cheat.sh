a025444b5c42591cf7fc287a300ee127acc6e025
Siddharth Reed <slreed@andrew.cmu.edu>
2022-01-20 11:41:38 -0500

    changed/fixed paths

--- .scripts/cheat.sh
+++ .scripts/cheat.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-cheat_sheet_dir="$HOME/.cheatsheets"
-vimconfig="$HOME/dotfiles/.config/nvim/cheat_vimrc"
-pdfgeom="=1000x800"
-termgeom="=100x50"
+CHEAT_SHEET_DIR="$HOME/.cheatsheets"
+VIMCONFIG="$HOME/.config/nvim/cheat_vimrc"
+PDFGEOM="=1000x800"
+TERMGEOM="=100x50"
 
 pick() {
-   sheet="$(find "$cheat_sheet_dir"/*.* -exec basename {} \; |\
+   sheet="$(find "$CHEAT_SHEET_DIR"/*.* -exec basename {} \; |\
             rev | cut -f2- -d'.' | rev |\
             rofi -m -1 -width 20 -lines 10 -dmenu)"
-   [[ -z "$sheet" ]] && echo "" || find "$cheat_sheet_dir"/*.* -name "$sheet.*"
+   [[ -z "$sheet" ]] && echo "" || find "$CHEAT_SHEET_DIR"/*.* -name "$sheet.*"
 }
 open() {
     sheet="$1"
     [[ -z "$sheet" ]] && echo "no selection" && exit
     ext="${sheet##*.}"
     case "$ext" in
-        pdf) tabbed -n 'cheatsheet' -g "$pdfgeom"  zathura "$sheet" -e ;;
-        txt|tex) st -n 'cheatsheet' -g "$termgeom" -e nvim -R -u "$vimconfig" "$sheet" ;;
-        md ) st -n 'cheatsheet' -g "$termgeom" -e sh -c "pandoc $sheet | w3m -T text/html" ;;
+        pdf) tabbed -n 'cheatsheet' -g "$PDFGEOM"  zathura "$sheet" -e ;;
+        txt|tex) st -n 'cheatsheet' -g "$TERMGEOM" -e nvim -R -u "$VIMCONFIG" "$sheet" ;;
+        md ) st -n 'cheatsheet' -g "$TERMGEOM" -e sh -c "pandoc $sheet | w3m -T text/html" ;;
         *) echo "Error: invalid extension in $1" && exit 1 ;;
     esac
     #i3-msg floating enable > /dev/null
