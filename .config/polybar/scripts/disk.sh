bb41f4f7646d9ff8072ab41e0a43333adf149acf
Siddharth Reed <slreed@andrew.cmu.edu>
2022-03-16 00:30:44 -0400

    automaticaly adjust display unit based on available size

--- .config/polybar/scripts/disk.sh
+++ .config/polybar/scripts/disk.sh
@@ -3,11 +3,11 @@ set -euo pipefail
 shopt -s extglob
 
 mode_file="$HOME/dotfiles/.config/polybar/modules.mode"
-modes=(percent amount used free) #amounts all
+modes=(percent amount used free amounts all)
 #icon=""
 
 help() {
-    echo "Error: usage ./$(basename $0) {display|next|prev|$(echo ${modes[*]} | tr ' ' '|')}"
+    echo "Error: usage ./$(basename ${0:-}) {display|next|prev|$(echo ${modes[*]} | tr ' ' '|')}"
 }
 cycle() {
     # cycle through modes either forwards or backwards
@@ -15,7 +15,7 @@ cycle() {
     # next mode index is:  (x+1) % n
     # prev mode index is:  (x+n-1) % n
     # x is current mode index, n is number of modes
-    dir="$1"
+    dir="${1:-}"
     mode="$(getMode)"
     idx="$(echo "${modes[*]}" | grep -o "^.*$mode" | tr ' ' '\n' | wc -l)"
     idx=$(($idx -1)) #current mode idx
@@ -31,7 +31,7 @@ getMode() {
     grep '^disk:' "$mode_file" | cut -d':' -f2
 }
 setMode() {
-    sed -i "/^disk:/s/:.*/:$1/" "$mode_file"
+    sed -i "/^disk:/s/:.*/:${1:-}/" "$mode_file"
 }
 
 disk() {
@@ -40,39 +40,43 @@ disk() {
     #-f2 is used disk space
     #-f3 is free disk space
     #-f4 is percent used
-    size="$(df | grep "$1$" | tr -s ' ' '\t' | cut -f2-5 | cut -f"$2")"
-    # # chose the scale for display and format
-    TB_size="$(echo 1 | numfmt --from-unit="Ti" --to-unit="Ki")"
-    GB3_size="$(echo 100 | numfmt --from-unit="Gi" --to-unit="Ki")"
-    case 1 in 
-        $(( $size > $TB_size )))
-            scale="Ti"
-            fmt="%.2f"
-            ;;
-        $(( $size > $GB3_size )))
-            scale="Gi"
-            fmt="%.1f"
-            ;;
-        *) 
-            scale="Gi"
-            fmt="%.2f"
-            ;;
-    esac
-    echo "$size ${scale}" | numfmt --from-unit=Ki --to-unit="$scale" --format "$fmt"
+    size="$(df | grep "${1:-}$" | tr -s ' ' '\t' | cut -f2-5 | cut -f"${2:-}")"
+    if [[ "$2" == 4 ]]; then
+        # just percentage, no scaling required
+        echo "$size"
+    else
+        # chose the scale for display and format
+        TB_size="$(echo 1 | numfmt --from-unit="Ti" --to-unit="Ki")"
+        GB_size="$(echo 100 | numfmt --from-unit="Gi" --to-unit="Ki")"
+        case 1 in 
+            $(($TB_size < $size)))
+                scale="Ti"
+                fmt="%.2f"
+                ;;
+            $(($GB_size < $size)))
+                scale="Gi"
+                fmt="%.1f"
+                ;;
+            *) 
+                scale="Gi"
+                fmt="%.2f"
+                ;;
+        esac
+        echo "$size ${scale}" | numfmt --from-unit=Ki --to-unit="$scale" --format "$fmt"
+    fi
 }
 percent_free() {
-    disk "$1" 4 \
+    disk "${1:-}" 4 \
         | sed -e 's/\([0-9]*\)%/100-\1/' \
         | bc \
         | sed -e 's/^\([0-9]\)$/0\1/' \
         | sed -e 's/$/%/'
 }
 display() {
-    mode="$1"
+    mode="${1:-}"
     msg=""
     dirs="$(df -h | grep '/dev/sd.. ' | grep -v 'efi' | rev | cut -d' ' -f1 | rev)"
     for dir in ${dirs[@]}; do
-
         case "$mode" in
             'percent') space="$(percent_free "$dir")" ;;
             'amount' ) space="$(disk "$dir" 3)" ;;
@@ -88,9 +92,9 @@ display() {
 }
 
 main() {
-    mode="$1"
+    mode="${1:-}"
     if [[ "$mode" == 'display' ]]; then
-        [[ -z "${2:-}" ]] && dmode="$(getMode)" || dmode="$2"
+        [[ -z "${2:-}" ]] && dmode="$(getMode)" || dmode="${2:-}"
         display "$dmode"
     else
         tmp="@($(echo ${modes[*]} | sed -e 's/ /|/g'))"
