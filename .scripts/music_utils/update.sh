cf1c98ef9f512c936e2c2f3e39b2f7bb6827c473
Siddharth Reed <slreed@andrew.cmu.edu>
2022-05-28 03:13:20 -0400

    moved to its own repo as a project to improve

--- .scripts/music_utils/update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-~/.scripts/music_utils/download-music.sh
-~/.scripts/music_utils/add-metadata.sh ~/Music/songs/*.mp3
-~/.scripts/music_utils/organize-music.sh ~/Music/songs/*.mp3
-~/.scripts/music_utils/playlists.sh
