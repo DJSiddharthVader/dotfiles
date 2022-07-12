4979f643ad3df4500e438c515729e8bd8f9714e1
Siddharth Reed <slreed@andrew.cmu.edu>
2022-07-12 17:10:46 -0400

    removed extract as it just didnt seem to work

--- .bash/bash-functions.sh
+++ .bash/bash-functions.sh
@@ -59,86 +59,19 @@ up() { # Goes up many dirs as the number passed as argument, if none goes up by 
     cd $d
 }
 extract() { # Archive Extractor {{{
-      clrstart="\033[1;34m"  # color codes
-      clrend="\033[0m"
-
-      if [[ "$#" -lt 1 ]]; then
-        echo -e "${clrstart}Pass a filename. Optionally a destination folder. You can also append a v for verbose output.${clrend}"
-        exit 1 # not enough args
-      fi
-
-      if [[ ! -e "$1" ]]; then
-        echo -e "${clrstart}File does not exist!${clrend}"
-        exit 2 # file not found
-      fi
-
-      if [[ -z "$2" ]]; then
-        DESTDIR="." # set destdir to current dir
-      elif [[ ! -d "$2" ]]; then
-        echo -e -n "${clrstart}Destination folder doesn't exist or isnt a directory. Create? (y/n): ${clrend}"
-        read response
-        #echo -e "\n"
-        if [[ $response == y || $response == Y ]]; then
-          mkdir -p "$2"
-          if [ $? -eq 0 ]; then
-            DESTDIR="$2"
-          else
-            exit 6 # Write perms error
-          fi
-        else
-          echo -e "${clrstart}Closing.${clrend}"; exit 3 # n/wrong response
-        fi
-      else
-        DESTDIR="$2"
-      fi
-
-      if [[ ! -z "$3" ]]; then
-        if [[ "$3" != "v" ]]; then
-          echo -e "${clrstart}Wrong argument $3 !${clrend}"
-          exit 4 # wrong arg 3
-        fi
-      fi
-
-      filename=`basename "$1"`
-      case "${filename#*.}" in
-        tar)
-          echo -e "${clrstart}Extracting $1 to $DESTDIR: (uncompressed tar)${clrend}"
-          tar x${3}f "$1" -C "$DESTDIR"
-          ;;
-        gz)
-          echo -e "${clrstart}Extracting $1 to $DESTDIR: (gip compressed tar)${clrend}"
-          tar x${3}fz "$1" -C "$DESTDIR"
-          ;;
-        tgz)
-          echo -e "${clrstart}Extracting $1 to $DESTDIR: (gip compressed tar)${clrend}"
-          tar x${3}fz "$1" -C "$DESTDIR"
-          ;;
-        xz)
-          echo -e "${clrstart}Extracting  $1 to $DESTDIR: (gip compressed tar)${clrend}"
-          tar x${3}f -J "$1" -C "$DESTDIR"
-          ;;
-        bz2)
-          echo -e "${clrstart}Extracting $1 to $DESTDIR: (bzip compressed tar)${clrend}"
-          tar x${3}fj "$1" -C "$DESTDIR"
-          ;;
-        zip)
-          echo -e "${clrstart}Extracting $1 to $DESTDIR: (zipp compressed file)${clrend}"
-          unzip "$1" -d "$DESTDIR"
-          ;;
-        rar)
-          echo -e "${clrstart}Extracting $1 to $DESTDIR: (rar compressed file)${clrend}"
-          unrar x "$1" "$DESTDIR"
-          ;;
-        7z)
-          echo -e  "${clrstart}Extracting $1 to $DESTDIR: (7zip compressed file)${clrend}"
-          7za e "$1" -o"$DESTDIR"
-          ;;
-        *)
-          echo -e "${clrstart}Unknown archieve format!"
-          exit 5
-          ;;
-      esac
-    }
+  filename=`basename "$1"`
+  case "${filename#*.}" in
+    tar) tar x${3}f "$1" -C "$DESTDIR" ;;
+    gz) tar x${3}fz "$1" -C "$DESTDIR" ;;
+    tgz) tar x${3}fz "$1" -C "$DESTDIR" ;;
+    xz) tar x${3}f -J "$1" -C "$DESTDIR" ;;
+    bz2) tar x${3}fj "$1" -C "$DESTDIR" ;;
+    zip) unzip "$1" -d "$DESTDIR" ;;
+    rar) unrar x "$1" "$DESTDIR" ;;
+    7z) 7za e "$1" -o"$DESTDIR" ;;
+    *) echo -e "${clrstart}Unknown archieve format!" ;;
+  esac
+}
 compress() {
 # Archive Compress {{{
       if [[ -n "$1" ]]; then
@@ -181,23 +114,6 @@ compress() { # Archive Compress
       else
         echo "usage: compress <foo.tar.gz> ./foo ./bar"
       fi
-}
-lowercase() { # move filenames to lowercase
-    for file ; do
-      filename=${file#*/}
-      case "$filename" in
-      */* ) dirname==${file%/*} ;;
-        * ) dirname=.;;
-      esac
-      nf=$(echo $filename | tr A-Z a-z)
-      newname="${dirname}/${nf}"
-      if [[ "$nf" != "$filename" ]]; then
-        mv "$file" "$newname"
-        echo "lowercase: $file --> $newname"
-      else
-        echo "lowercase: $file not changed."
-      fi
-    done
 }
 dirsize () { # List sub-directory sizes in pwd
     du -shx * .[a-zA-Z0-9_]* 2> /dev/null | egrep '^ *[0-9.]*[MG]' | sort -n > /tmp/list
