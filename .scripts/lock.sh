0b8edc1e6d51cb0205dfaa00d9ffb60daefca67f
DJSiddharthVader <sidreed34@gmail.com>
2023-08-10 16:03:25 -0400

    heavily cleaned up

--- .scripts/lock.sh
+++ .scripts/lock.sh
@@ -1,30 +1,13 @@
 #!/bin/bash
 set -e
+# Variables
 SCREEN="$HOME/.varfiles/screen.png"
-# source curremt theme colors
+FONT="GohuFont 11 Nerd Font Mono:style=Regular:size=18"
+DATE="$(date +'%A, %B %d %Y')"
+# source current theme colors
 tmp="$(mktemp)"
 tail -n+4 $HOME/.cache/wal/colors.sh >| $tmp
 source $tmp
-# set colors for lock screen elements
-font="GohuFont 11 Nerd Font Mono:style=Regular:size=18"
-locktext="$(date +'%A, %B %d %Y' )"
-alpha="ff"
-background="$(echo "$color1" | tr -d '#')"
-background="$color1"
-ringcolor="$(echo "$color2$alpha" | tr -d '#')"
-keyhlcolor="$(echo "$color3$alpha" | tr -d '#')"
-bshlcolor="$(echo "$color4$alpha" | tr -d '#')"
-insidecolor=00000000
-separatorcolor=00000000
-wrongcolor="$(echo "$color5$alpha" | tr -d '#')"
-ringwrongcolor="$(echo "$color5$alpha" | tr -d '#')"
-insidewrongcolor=00000000
-verifcolor="$(echo "$color6$alpha" | tr -d '#')"
-ringvercolor="$(echo "$color6$alpha" | tr -d '#')"
-insidevercolor=00000000
-timecolor="$(echo "$foreground$alpha" | tr -d '#')"
-datecolor="$(echo "$foreground$alpha" | tr -d '#')"
-loginbox="$(echo "$color1 66" | tr -d ' #')"
 lockbox() {
     #drawing rectangles
     rectangles=" "
@@ -42,43 +25,46 @@ main(){
     mpc pause > /dev/null 2>&1
     # bw lock # lock bitwarden
     ~/.config/polybar/scripts/pulseaudio-control.sh mute
-    #~/.scripts/mullvad.sh disconnect
-    #prep lockscreen
+    # prep lockscreen
     rectangles="$(lockbox)"
     scrot -o "$SCREEN"
-    convert "$SCREEN" -scale 20% -scale 500% -draw "fill $background fill-opacity 0.8 $rectangles" "$SCREEN"
+    convert "$SCREEN" \
+            -scale 20% \
+            -scale 500% \
+            -draw "fill $color1 fill-opacity 0.85 $rectangles" \
+            "$SCREEN"
     #lock command
     i3lock                                    \
         -i "$SCREEN"                          \
-        -t                                    \
-        --ind-pos='x+290:h-80'                \
-        --noinput-text=''                     \
+        -e -f                                 \
         --clock                               \
-        --time-pos='x+115:h-80'               \
-        --time-color="$timecolor"             \
-        --time-font="$font"                   \
-        --date-str "$locktext"                \
-        --date-pos='x+145:h-60'               \
-        --date-color="$datecolor"             \
-        --date-font="$font"                   \
-        --layout-font="$font"                 \
-        --radius=25                           \
-        --ring-width=4                        \
-        --ring-color=$ringcolor               \
-        --inside-color=$insidecolor           \
-        --line-uses-inside                    \
-        --keyhl-color=$keyhlcolor             \
-        --bshl-color=$bshlcolor               \
-        --separator-color=$separatorcolor     \
+        --radius=29                           \
+        --ring-width=7                        \
+        --ind-pos='x+290:h-80'                \
+        --time-pos='x+145:h-80'               \
+        --date-pos='x+145:h-55'               \
+        --noinput-text=''                     \
         --verif-text=''                       \
-        --verif-font="$font"                  \
-        --verif-color="$verifcolor"           \
-        --ringver-color=$ringvercolor         \
-        --insidever-color=$insidevercolor     \
-        --wrong-text=''                       \
-        --wrong-font="$font"                  \
-        --wrong-color="$wrongcolor"           \
-        --ringwrong-color=$ringwrongcolor     \
-        --insidewrong-color=$insidewrongcolor
+        --wrong-text='!!!'                    \
+        --date-str="$DATE"                    \
+        --layout-font="$FONT"                 \
+        --time-font="$FONT"                   \
+        --date-font="$FONT"                   \
+        --verif-font="$FONT"                  \
+        --wrong-font="$FONT"                  \
+        --time-color="$foreground"            \
+        --date-color="$foreground"            \
+        --ring-color=$color2                  \
+        --keyhl-color=$color3                 \
+        --bshl-color=$color4                  \
+        --wrong-color=$color5                 \
+        --ringwrong-color=$color5             \
+        --verif-color=$color6                 \
+        --ringver-color=$color6               \
+        --line-uses-inside                    \
+        --inside-color=00000000               \
+        --insidever-color=00000000            \
+        --separator-color=00000000            \
+        --insidewrong-color=00000000
 }
 main
