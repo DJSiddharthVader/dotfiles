738ccca718b2b16f9a10c6dafbd17f1dc7ee3afe
Siddharth Reed <slreed@andrew.cmu.edu>
2024-11-16 16:08:59 -0500

    update how temp is fetched

--- .config/polybar/scripts/temperature.sh
+++ .config/polybar/scripts/temperature.sh
@@ -45,10 +45,8 @@ setMode() {
 }
 
 get_temp() {
-    sensors \
-        |& grep -v 'ERROR' \
-        | grep Package \
-        | sed -e 's/^.*: *+\([0-9]*\)\..*$/\1/'
+    # sensors | grep Composite | cut -d'C' -f3 | grep -o [0-9\.] | paste -sd '' | cut -d'.' -f1
+    sensors | grep 'Sensor 1' | cut -d':' -f2 | cut -d'(' -f1 | grep -o [0-9\.] | paste -sd '' | cut -d'.' -f1
 }
 get_icon() {
     temp="$(get_temp)"
