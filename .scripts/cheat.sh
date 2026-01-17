ebe5dbe9d420a1bab060e34027b27326935f3718
Siddharth Reed <slreed@andrew.cmu.edu>
2026-01-16 19:38:30 -0500

    remove hardcoded term geometry

--- .scripts/cheat.sh
+++ .scripts/cheat.sh
@@ -2,7 +2,8 @@
 CHEAT_SHEET_DIR="$HOME/.cheatsheets"
 # VIMCONFIG="$HOME/.config/nvim/cheat_vimrc"
 PDFGEOM="=1000x800"
-TERMGEOM="=80x50"
+ST_PATH="st"
+EDITOR_CMD="${HOME}/bin/nvim-linux-x86_64/bin/nvim"
 
 pick() {
    sheet="$(find "$CHEAT_SHEET_DIR"/*.* -exec basename {} \; |\
@@ -10,22 +11,20 @@ pick() {
             rofi -m -1 -width 20 -lines 10 -dmenu)"
    [[ -z "$sheet" ]] && echo "" || find "$CHEAT_SHEET_DIR"/*.* -name "$sheet.*"
 }
+
 open() {
     sheet="$1"
     [[ -z "$sheet" ]] && echo "no selection" && exit
     ext="${sheet##*.}"
     case "$ext" in
-        pdf) tabbed -n 'cheatsheet' -g "$PDFGEOM"  zathura "$sheet" -e ;;
-        txt|tex) st -n 'cheatsheet' -g "$TERMGEOM" -e nvim -R -u "$VIMCONFIG" "$sheet" ;;
-        sh) st -n 'cheatsheet' -g "$TERMGEOM" -e nvim -R "$sheet" ;;
-        md ) st -n 'cheatsheet' -g "$TERMGEOM" -e sh -c "pandoc $sheet | w3m -T text/html" ;;
+        pdf    ) tabbed -n 'cheatsheet' -g "$PDFGEOM"  zathura "$sheet" -e ;;
+        txt|tex) ${ST_PATH} -n 'cheatsheet' -e ${EDITOR_CMD} -R "$sheet" ;;
+        sh     ) ${ST_PATH} -n 'cheatsheet' -e ${EDITOR_CMD} -R "$sheet" ;;
+        md     ) ${ST_PATH} -n 'cheatsheet' -e sh -c "pandoc $sheet | w3m -T text/html" ;;
         *) echo "Error: invalid extension in $1" && exit 1 ;;
     esac
     #i3-msg floating enable > /dev/null
     #i3-msg move position center > /dev/null
 }
-main() {
-    open "$(pick)"
-}
 
-main
+open "$(pick)"
