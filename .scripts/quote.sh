5c214a018f3374b46e761d6f2b1d2ca5aa4ede49
DJSiddharthVader <sidreed34@gmail.com>
2026-09-03 16:22:05 -0400

    remove pr dependency because of issue with printing, change offset to be integer counts

--- .scripts/quote.sh
+++ .scripts/quote.sh
@@ -2,24 +2,25 @@
 # Data
 # all quotes start with " and have an author on the next line, no other lines should start with a "
 # quote_files=(~/.varfiles/quotes.md ~/.varfiles/quotes.txt)
-quote_files=(~/.varfiles/quotes.md)
+# quote_files=('/home/vizier/Documents/Vizier\'s Codex/Thinkies/Quotes.md')
+quote_file="/home/vizier/Documents/Vizier's Codex/Thinkies/Quotes.md"
 # dir with ascii art for various thinkers
 thinker_dir="$HOME/dotfiles/.ascii/thinkers" 
 # Default Args
 colwrap=50
 border_string="(~)~"
-thinker_offset="0.33"
-thinker_line=2
+thinker_line_offset=10
+thinker_line=3
 
 help() {
     thinker_list="$(find $thinker_dir -type f -name "*.ascii" -exec basename {} \; | cut -d'.' -f1 | tr '\n' '|' | sed -e 's/|$//' )"
     echo "Print a quote and something thinking it (basically fortune | cowsay)
 Usage: $0 [OPTIONS]
         -p      print only the random quote (plain)
-        -q      quote (string, defualt is random from ${quote_files[@]})
+        -q      quote (string, defualt is random from ${quote_file})
         -c      column wrap width for quote text (int, default $colwrap)
         -b      border string specifiying bubble boundaries (left top right bottom, no spaces, default is '$border_string')
-        -o      thinker offset from left side (float in [0,1], default is $thinker_offset)
+        -o      thinker offset from left side (float in [0,1], default is $thinker_line_offset)
         -l      length of diagonal line connecting thinker to text box (int, default is $thinker_line)
         -t      thinker ascii art (file, defualt is random file from $thinker_dir)
                 possible thinkers are {$thinker_list} or any filepath
@@ -28,8 +29,8 @@ Usage: $0 [OPTIONS]
 
 random_quote() {
     # pick random quote from local files
-    quote="$(cat ${quote_files[@]} | grep '^"' | shuf -n1 | tr -d '"')"
-    author="$(cat ${quote_files[@]} | grep -A1 "$quote" | tail -1 )"
+    quote="$(cat "${quote_file}" | grep '^"' | shuf -n1 | tr -d '"')"
+    author="$(cat "${quote_file}" | grep -A1 "$quote" | tail -1 )"
     if [[ -z $author ]]; then
         echo "$quote"
     else
@@ -41,7 +42,7 @@ get_width() {
     # get width  of longest line (not including trailing spaces) of quote when wrapped to colwrap chars
     quote_string="$1"
     colwrap="$2"
-    echo -e "$quote_string" | fold -w $colwrap -s | awk '{ print length }' | sort -n | tail -1
+    echo -e "${quote_string}" | fold -w ${colwrap} -s | awk '{ print length }' | sort -n | tail -1
 }
 
 bubble() {
@@ -56,26 +57,35 @@ bubble() {
     right_rule="${border_string:2:1}"
     bottom_rule="${border_string:3:1}"
     # Print top rule
-    printf "%.0s$top_rule" $(seq 1 $box_width) | sed -e 's/^/ /' -e 's/$/ \n/'
+    printf "%.0s${top_rule}" $(seq 1 ${box_width}) | sed -e 's/^/ /' -e 's/$/ \n/'
     # Print wraped quote and author surrounded by though bubble
-    printf "%-${colwrap}s\n" "$(echo -e "$quote_string")" |\
-            fold -w $(($box_width)) -s |\
-            sed -e "s/^/$left_rule/" -e "s/$/~$right_rule/" |\
+    printf "%-${colwrap}s\n" "$(echo -e "${quote_string}")" |\
+            fold -w $((${box_width})) -s |\
+            sed -e "s/^/${left_rule}/" -e "s/$/~${right_rule}/" |\
             column -s'~' -t -c2 |\
-            sed -e "s/\s\s$right_rule/$right_rule/"
+            sed -e "s/\s\s${right_rule}/${right_rule}/"
     # Print bottom rule
-    printf "%.0s$bottom_rule" $(seq 1 $box_width) | sed -e 's/^/ /' -e 's/$/ \n/'
+    printf "%.0s${bottom_rule}" $(seq 1 ${box_width}) | sed -e 's/^/ /' -e 's/$/ \n/'
 }
 
 thinker() {
-    # Print text line and thinker, padding it to be positioned at some offset under the quote bubble
-    thinker_file="$1" # ascii art file for the thinker
-    thinker_offset="$2" # centering of spearker below text box, 0 is fully left justified and 1 is fully right justified, 0.5 mean that the left-most char of the thinker text aligns at the 50% mark the quote text
-    thinker_line="$3" # length of line connecting thinker to bubble
-    thinker_padding="$4" # how far to push the thinker image left
-    if [[ $thinker_line -gt 0 ]]; then
-        for i in $(seq $thinker_line -1 1); do
-            echo '\' | pr -T -o $(($thinker_padding-$i))
+    # Print text line and thinker, padding by thinker_line_offset characters
+    thinker_file="$1"    # ascii art file for the thinker
+    thinker_line_offset="$2" 
+    thinker_line="$3"    # length of line connecting thinker to bubble
+    thinker_width=$(wc -L "${thinker_file}" | cut -d' ' -f1)
+    # total_width=$((${thinker_line_offset}+${thinker_width}))
+    # if [[ ${total_width} -gt ${COLUMNS} ]]; then
+    if [[ $((${thinker_line_offset}+${thinker_width})) -gt ${COLUMNS} ]]; then
+        thinker_line_offset=$((${COLUMNS}-${thinker_width}-1))
+    fi
+    # print whatever is thinking the bubble
+    if [[ ${thinker_line} -gt 0 ]]; then
+        # for i in $(seq ${thinker_line} -1 1); do
+        for i in $(seq 0 1 $((${thinker_line}-1)) ); do
+            padding=$((${i}+${thinker_line_offset}))
+            printf "%${padding}s\ \n"
+            # echo '\' | pr -T -o $((${thinker_padding}-${i}))
         done
         # incrementally add spaces before a \ char, so a value of 3 would be
         #bubble
@@ -85,7 +95,12 @@ thinker() {
         #{       }| speaker art
         #{} is $thiknker_padding chars long
     fi
-    pr -T -o $thinker_padding $thinker_file # "push" (left pad with spaces) thinker left
+    # "push" (left pad with spaces) thinker left
+    # cat "${thinker_file}" 
+    # while read i; do printf "%${thinker_line_offset}s ${i} \n"; done < "${thinker_file}"
+    while IFS= read -r line; do 
+        printf "%${thinker_line_offset}s${line}\n"; 
+    done < "${thinker_file}"
 }
 
 display() {
@@ -94,16 +109,13 @@ display() {
     colwrap="$2"
     border_string="$3"
     thinker_file="$4"
-    thinker_offset="$5"
+    thinker_line_offset="$5"
     thinker_line="$6"
     # get width of thought bubble
-    box_width="$(get_width "$quote_string" "$colwrap")"
+    box_width="$(get_width "${quote_string}" "${colwrap}")"
     # print thought bubble wraped and decorated
-    bubble "$quote_string" $colwrap $box_width "$border_string"
-    # left offestt of thinker vs. bubble
-    thinker_padding="$(echo "$box_width*$thinker_offset+1" | bc | cut -d'.' -f1)"
-    # print whatever is thinking the bubble
-    thinker $thinker_file $thinker_offset $thinker_line $thinker_padding
+    bubble "${quote_string}" ${colwrap} ${box_width} "${border_string}"
+    thinker ${thinker_file} ${thinker_line_offset} ${thinker_line}
 }
 
 main() {
@@ -118,7 +130,7 @@ main() {
             c) colwrap="$OPTARG"               ;;
             b) border_string="$OPTARG"         ;;
             l) thinker_line="$OPTARG"          ;;
-            o) thinker_offset="$OPTARG"        ;;
+            o) thinker_line_offset="$OPTARG"        ;;
             p) echo -e "$quote_string" && exit 0 ;;
             t) if [ -f "$OPTARG" ]; then
                     thinker_file="$OPTARG"
@@ -138,7 +150,7 @@ main() {
     done
     shift "$((OPTIND-1))"
     [[ -n "$author" ]] && quote_string="$quote_string\n --$author"
-    display "$quote_string" $colwrap $border_string $thinker_file $thinker_offset $thinker_line
+    display "${quote_string}" ${colwrap} ${border_string} ${thinker_file} ${thinker_line_offset} ${thinker_line}
 }
 
 main "$@"
